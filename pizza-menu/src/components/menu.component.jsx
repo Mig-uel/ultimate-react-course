6236c1fa942a29b4ae755afaea0111c1afc422a2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-04 12:09:04 -0400

    Refactor Menu component to display pizza items in a list

--- pizza-menu/src/components/menu.component.jsx
+++ pizza-menu/src/components/menu.component.jsx
@@ -1,4 +1,15 @@
+import Pizza from './pizza.component'
+
 const Menu = () => {
-  return <div>Menu</div>
+  return (
+    <div>
+      <h2>Our Menu</h2>
+
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  )
 }
 export default Menu
