74b13fef6f7a489bbd8b2889c1e17364b25013f5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-06 16:46:18 -0500

    Add business hours logic to App component

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -4,6 +4,18 @@ import Header from './components/header.component'
 import Menu from './components/menu.component'
 
 function App() {
+  const hour = new Date().getHours()
+  const openHour = 12
+  const closeHour = 22
+
+  const isOpen = hour >= openHour && hour <= closeHour
+
+  console.log(isOpen)
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert('We are currently open!')
+  // } else alert('We are currently closed!')
+
   return (
     <div>
       <Header />
