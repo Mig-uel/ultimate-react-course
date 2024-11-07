bb4dd93e8e2a0ffff5e8b50b5345a5c6a06a99a7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-07 16:56:12 -0500

    Update Pizza component to replace 'Pizza Spinaci' with 'Pizza Funghi' and adjust ingredients, photo, and price

--- pizza-menu/src/components/menu.component.jsx
+++ pizza-menu/src/components/menu.component.jsx
@@ -13,10 +13,10 @@ const Menu = () => {
       />
 
       <Pizza
-        name='Pizza Spinaci'
-        ingredients='Tomato, mozzarella, spinach, and ricotta cheese'
-        photo='https://images.unsplash.com/photo-1617343251257-b5d709934ddd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        price='10'
+        name='Pizza Funghi'
+        ingredients='Tomato, mushrooms'
+        photo='https://images.unsplash.com/photo-1717883235373-ef10b2a745a3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        price='8'
       />
     </main>
   )
