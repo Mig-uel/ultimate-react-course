d800ee9c57b92cc846e7997735c7daa2b2bd52de
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-07 16:48:53 -0500

    Refactor Menu component to use semantic HTML and add Pizza items with details

--- pizza-menu/src/components/menu.component.jsx
+++ pizza-menu/src/components/menu.component.jsx
@@ -2,14 +2,23 @@ import Pizza from './pizza.component'
 
 const Menu = () => {
   return (
-    <div>
+    <main className='menu'>
       <h2>Our Menu</h2>
 
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozzarella, spinach, and ricotta cheese'
+        photo='https://images.unsplash.com/photo-1617343251257-b5d709934ddd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        price='10'
+      />
+
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozzarella, spinach, and ricotta cheese'
+        photo='https://images.unsplash.com/photo-1617343251257-b5d709934ddd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        price='10'
+      />
+    </main>
   )
 }
 export default Menu
