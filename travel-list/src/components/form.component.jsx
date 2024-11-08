f89edcd548c859d0dc5cbf0938f89aa6ad33b2af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 16:24:05 -0500

    Add quantity selection and description input to Form component

--- travel-list/src/components/form.component.jsx
+++ travel-list/src/components/form.component.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
+
 const Form = () => {
+  const [qty, setQty] = useState(1)
+  const [description, setDescription] = useState('')
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    setQty(1)
+    setDescription('')
   }
 
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
 
-      <select>
+      <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
         {Array.from({ length: 20 }, (curr, index) => index + 1).map((i) => (
           <option key={i} value={i}>
             {i}
@@ -15,8 +23,13 @@ const Form = () => {
         ))}
       </select>
 
-      <input type='text' placeholder='Item...' />
-      <button type='button'>Add</button>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type='submit'>Add</button>
     </form>
   )
 }
