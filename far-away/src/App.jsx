bbf70db3c6fa7504ca093e725ecf3999c41df975
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 11:31:44 -0500

    Refactor App component to implement step-based messaging and navigation buttons

--- far-away/src/App.jsx
+++ far-away/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+const messages = [
+  'Learn React 🧪',
+  'Apply for Jobs 💼',
+  'Invest your new income 🤑',
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const step = 1
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={`${step === 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step === 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step === 3 ? 'active' : ''}`}>3</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+
+      <p className='message'>
+        Step {step}: {messages[step - 1]}
       </p>
-    </>
+
+      <div className='buttons'>
+        <button
+          style={{
+            backgroundColor: '#7950F2',
+            color: 'white',
+          }}
+        >
+          Previous
+        </button>
+        <button
+          style={{
+            backgroundColor: '#7950F2',
+            color: 'white',
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   )
 }
 
