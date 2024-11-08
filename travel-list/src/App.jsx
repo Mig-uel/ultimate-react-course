99b71792630c6efa596c38f00b0da9a79d0e439e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 16:31:31 -0500

    Refactor App component to use state for items management

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,15 +1,16 @@
+import { useState } from 'react'
 import Form from './components/form.component'
 import Logo from './components/logo.component'
 import PackingList from './components/packing-list.component'
 import Stats from './components/stats.component'
 
-const items = [
-  { id: 1, description: 'Passports', qty: 2, packed: false },
-  { id: 2, description: 'Socks', qty: 12, packed: false },
-  { id: 3, description: 'Charger', qty: 1, packed: true },
-]
-
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, description: 'Passports', qty: 2, packed: false },
+    { id: 2, description: 'Socks', qty: 12, packed: false },
+    { id: 3, description: 'Charger', qty: 1, packed: true },
+  ])
+
   return (
     <div className='app'>
       <Logo />
