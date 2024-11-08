6ad575103496c0a548701793acac1ac2f042ea1d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-07 21:13:05 -0500

    Add SOLD OUT indication to Pizza component when unavailable

--- pizza-menu/src/components/pizza.component.jsx
+++ pizza-menu/src/components/pizza.component.jsx
@@ -1,15 +1,13 @@
 const Pizza = ({ name, ingredients, photo, price, soldOut }) => {
   return (
-    <li
-      className='pizza'
-      style={{ textDecoration: `${soldOut ? 'line-through' : ''}` }}
-    >
+    <li className='pizza'>
       <img src={photo} alt={name} />
 
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+
+        {soldOut ? <span>SOLD OUT</span> : <span>{price}</span>}
       </div>
     </li>
   )
