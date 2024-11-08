3e226eed3910e4bca631ef90476ac5f1b2012033
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 16:01:30 -0500

    Add charger item to initial packing list in App component

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -6,6 +6,7 @@ import Stats from './components/stats.component'
 const items = [
   { id: 1, description: 'Passports', qty: 2, packed: false },
   { id: 2, description: 'Socks', qty: 12, packed: false },
+  { id: 3, description: 'Charger', qty: 1, packed: true },
 ]
 
 function App() {
