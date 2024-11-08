41adf3706ed47db7f34722c247c6f538989c09e0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 15:58:17 -0500

    Refactor PackingList component to render items dynamically and add PropTypes validation

--- travel-list/src/components/packing-list.component.jsx
+++ travel-list/src/components/packing-list.component.jsx
@@ -1,4 +1,19 @@
-const PackingList = () => {
-  return <div className='list'>LIST</div>
+import Item from './item.component'
+import PropTypes from 'prop-types'
+
+const PackingList = ({ items }) => {
+  return (
+    <div className='list'>
+      <ul>
+        {items.map((item) => (
+          <Item {...item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
 }
 export default PackingList
+
+PackingList.propTypes = {
+  items: PropTypes.array,
+}
