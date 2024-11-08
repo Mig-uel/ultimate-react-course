a3c6bbba1acf64fc4c36e3cdf12914a6f8091a0c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 15:52:50 -0500

    Add initial packing list items to App component

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -3,12 +3,17 @@ import Logo from './components/logo.component'
 import PackingList from './components/packing-list.component'
 import Stats from './components/stats.component'
 
+const items = [
+  { id: 1, description: 'Passports', qty: 2, packed: false },
+  { id: 2, description: 'Socks', qty: 12, packed: false },
+]
+
 function App() {
   return (
     <div className='app'>
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList items={items} />
       <Stats />
     </div>
   )
