fbbf333f3a9c732d946dc086022dc2096b298c2b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 19:18:46 -0500

    Implement item packing state and delete functionality in Item component

--- travel-list/src/components/item.component.jsx
+++ travel-list/src/components/item.component.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-const Item = ({ description, packed, qty }) => {
+const Item = ({ id, description, packed, qty, setItems }) => {
+  const [isPacked, setIsPacked] = useState(packed)
+
+  const handleCheck = () => setIsPacked((prev) => !prev)
+
+  const handleDelete = (id) => {
+    setItems((prev) => prev.filter((item) => item.id !== id))
+  }
+
   return (
     <li>
+      <input type='checkbox' checked={isPacked} onChange={handleCheck} />
+
       <span
         style={
-          packed
+          isPacked
             ? {
                 textDecoration: 'line-through',
               }
@@ -14,6 +25,8 @@ const Item = ({ description, packed, qty }) => {
       >
         {qty} &times; {description}
       </span>
+
+      <button onClick={() => handleDelete(id)}>❌</button>
     </li>
   )
 }
@@ -24,4 +37,5 @@ Item.propTypes = {
   description: PropTypes.string,
   packed: PropTypes.bool,
   qty: PropTypes.number,
+  setItems: PropTypes.func,
 }
