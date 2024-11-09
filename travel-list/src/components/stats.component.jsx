4f36ac435571124ba969806f50a47c5f0bcd5bdc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-08 19:42:33 -0500

    Enhance Stats component to display packing progress and add prop types validation

--- travel-list/src/components/stats.component.jsx
+++ travel-list/src/components/stats.component.jsx
@@ -1,8 +1,19 @@
-const Stats = () => {
+import PropTypes from 'prop-types'
+
+const Stats = ({ itemLength, percentage, numOfItemsPacked }) => {
   return (
     <footer className='stats'>
-      <em>You have X items on your list, and you already packed X (x%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything packed! Ready to go ✈️'
+          : ` You have ${itemLength} items on your list, and you already packed ${numOfItemsPacked} items (${percentage}%)`}
+      </em>
     </footer>
   )
 }
 export default Stats
+Stats.propTypes = {
+  itemLength: PropTypes.number,
+  percentage: PropTypes.number,
+  numOfItemsPacked: PropTypes.number,
+}
