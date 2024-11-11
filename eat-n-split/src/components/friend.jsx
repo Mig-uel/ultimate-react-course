594f9ca25d2f0003392b1709d96c0f8a7bed2bfd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-10 21:22:23 -0500

    Enhance Friend component to display image, name, and balance status

--- eat-n-split/src/components/friend.jsx
+++ eat-n-split/src/components/friend.jsx
@@ -1,7 +1,24 @@
 import PropTypes from 'prop-types'
 
 const Friend = ({ name, image, balance }) => {
-  return <li>{name}</li>
+  return (
+    <li>
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+
+      {balance > 0 ? (
+        <p className='green'>
+          {name} owes you ${balance}
+        </p>
+      ) : balance === 0 ? (
+        <p>You and {name} are even</p>
+      ) : (
+        <p className='red'>
+          You owe {name} ${Math.abs(balance)}
+        </p>
+      )}
+    </li>
+  )
 }
 export default Friend
 Friend.propTypes = {
