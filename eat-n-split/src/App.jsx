a0376cf74f280e6cbbf55b8cac83d8fc43252329
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-10 21:14:20 -0500

    Add FriendsList component and initialize friends state in App component

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react'
+import FriendsList from './components/friends-list'
+
 function App() {
-  return <></>
+  const [friends, setFriends] = useState([
+    {
+      id: 118836,
+      name: 'Clark',
+      image: 'https://i.pravatar.cc/48?u=118836',
+      balance: -7,
+    },
+    {
+      id: 933372,
+      name: 'Sarah',
+      image: 'https://i.pravatar.cc/48?u=933372',
+      balance: 20,
+    },
+    {
+      id: 499476,
+      name: 'Anthony',
+      image: 'https://i.pravatar.cc/48?u=499476',
+      balance: 0,
+    },
+  ])
+  return (
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList friends={friends} />
+      </div>
+    </div>
+  )
 }
 
 export default App
