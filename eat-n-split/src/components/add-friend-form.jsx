52ca514988ef06209076a229acbc67d2a40cdba2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-10 21:54:53 -0500

    Implement AddFriendForm with state management and PropTypes validation

--- eat-n-split/src/components/add-friend-form.jsx
+++ eat-n-split/src/components/add-friend-form.jsx
@@ -1,13 +1,44 @@
+import PropTypes from 'prop-types'
 import Button from './button'
+import { useState } from 'react'
 
-const AddFriendForm = () => {
+const AddFriendForm = ({ addFriendHandler }) => {
+  const [name, setName] = useState('')
+  const [image, setImage] = useState(`https://robohash.org/${Date.now()}`)
+
+  const addFriend = (e) => {
+    e.preventDefault()
+
+    if (!name || !image) return
+
+    const friend = {
+      id: Date.now(),
+      name,
+      image,
+      balance: 0,
+    }
+
+    addFriendHandler(friend)
+
+    setName('')
+    setImage(`https://robohash.org/${Date.now()}`)
+  }
+
   return (
-    <form className='form-add-friend'>
+    <form className='form-add-friend' onSubmit={addFriend}>
       <label>🧑Friend Name</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🖼️ Image URL</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add Friend</Button>
     </form>
@@ -15,3 +46,6 @@ const AddFriendForm = () => {
 }
 
 export default AddFriendForm
+AddFriendForm.propTypes = {
+  addFriendHandler: PropTypes.func,
+}
