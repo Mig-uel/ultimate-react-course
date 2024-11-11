c026b466bbda9845c79ee52ad19cdb541d69c4da
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-10 21:41:06 -0500

    Update labels and button text in AddFriendForm for clarity

--- eat-n-split/src/components/add-friend-form.jsx
+++ eat-n-split/src/components/add-friend-form.jsx
@@ -3,14 +3,15 @@ import Button from './button'
 const AddFriendForm = () => {
   return (
     <form className='form-add-friend'>
-      <label>Friends Name</label>
+      <label>🧑Friend Name</label>
       <input type='text' />
 
-      <label>User Image URL</label>
+      <label>🖼️ Image URL</label>
       <input type='text' />
 
-      <Button>Add</Button>
+      <Button>Add Friend</Button>
     </form>
   )
 }
+
 export default AddFriendForm
