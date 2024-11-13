36d564dc1a56ad0bb58028116143bb278602e569
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 17:40:37 -0500

    Refactor Nav component to use separate Search component for improved structure

--- usePopcorn/src/components/nav/nav.component.jsx
+++ usePopcorn/src/components/nav/nav.component.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
+import Search from './search.component'
 
 const Nav = () => {
-  const [query, setQuery] = useState('')
-
   return (
     <nav className='nav-bar'>
       <div className='logo'>
         <span role='img'>🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className='search'
-        type='text'
-        placeholder='Search movies...'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+
+      <Search />
+
       <p className='num-results'>
         Found <strong>X movies</strong> results
       </p>
