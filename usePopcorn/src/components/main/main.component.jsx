81dae95b8bbdeaee9b72e13d6c48c8bf0a331525
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 18:54:20 -0500

    Refactor Main component to use children for composition instead of hardcoded ResultsBox and WatchedBox

--- usePopcorn/src/components/main/main.component.jsx
+++ usePopcorn/src/components/main/main.component.jsx
@@ -1,15 +1,5 @@
-import ResultsBox from './results-box.component'
-import WatchedBox from './watched-box.component'
-
-const Main = ({ movies }) => {
-  return (
-    <main className='main'>
-      {/* SEARCH RESULTS BOX */}
-      <ResultsBox movies={movies} />
-
-      {/* WATCHED BOX */}
-      <WatchedBox />
-    </main>
-  )
+// COMPONENT COMPOSITION
+const Main = ({ children }) => {
+  return <main className='main'>{children}</main>
 }
 export default Main
