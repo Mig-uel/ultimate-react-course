049fefb8649c542396ca3b234f7694e8e7f07fdd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 18:50:17 -0500

    Refactor App component to include Logo and Search components within Nav, and pass movies to NumResults

--- usePopcorn/src/App.jsx
+++ usePopcorn/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 import { tempMovieData } from './db'
+
+// NAV COMPONENTS
 import Nav from './components/nav/nav.component'
+import Logo from './components/nav/logo.component'
+import Search from './components/nav/search.component'
+import NumResults from './components/nav/num-results.component'
+
+// MAIN COMPONENTS
 import Main from './components/main/main.component'
 
 export default function App() {
@@ -8,7 +15,16 @@ export default function App() {
 
   return (
     <>
-      <Nav movies={movies} />
+      <Nav>
+        {/* LOGO */}
+        <Logo />
+
+        {/* SEARCH BAR */}
+        <Search />
+
+        {/* NUM RESULTS */}
+        <NumResults movies={movies} />
+      </Nav>
 
       <Main movies={movies} />
     </>
