24140660132d146bcf76a4349d788a7ba8afedf8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 14:45:44 -0500

    Pass selectedFriend prop to FriendsList and update Friend component for selection highlighting

--- eat-n-split/src/components/friends-list.jsx
+++ eat-n-split/src/components/friends-list.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types'
 import Friend from './friend'
 
-const FriendsList = ({ friends, onSelect }) => {
+const FriendsList = ({ friends, onSelect, selectedFriend }) => {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend key={friend.id} {...friend} onSelect={onSelect} />
+        <Friend
+          key={friend.id}
+          {...friend}
+          onSelect={onSelect}
+          selectedFriend={selectedFriend}
+        />
       ))}
     </ul>
   )
@@ -14,4 +19,5 @@ export default FriendsList
 FriendsList.propTypes = {
   friends: PropTypes.array,
   onSelect: PropTypes.func,
+  selectedFriend: PropTypes.object,
 }
