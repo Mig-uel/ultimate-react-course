1a9e119993a037b9f14930105f939e0763b94683
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 15:15:06 -0500

    Add onSplitBill prop to SplitBillForm and implement bill splitting logic

--- eat-n-split/src/components/split-bill-form.jsx
+++ eat-n-split/src/components/split-bill-form.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 import Button from './button'
 
-const SplitBillForm = ({ selectedFriend }) => {
+const SplitBillForm = ({ selectedFriend, onSplitBill }) => {
+  const [bill, setBill] = useState('')
+  const [paidByUser, setPaidByUser] = useState('')
+  const paidByFriend = bill && paidByUser <= bill ? bill - paidByUser : 0
+  const [whoIsPaying, setWhoIsPaying] = useState('user')
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (!bill || !paidByUser) return
+
+    onSplitBill(whoIsPaying === 'user' ? paidByFriend : -paidByUser)
   }
 
   return (
@@ -11,16 +21,27 @@ const SplitBillForm = ({ selectedFriend }) => {
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💸 Bill Value</label>
-      <input type='text' />
+      <input
+        type='number'
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>🤵 Your Expense</label>
-      <input type='text' />
+      <input
+        type='number'
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(Number(e.target.value))}
+      />
 
-      <label>🧑‍🤝‍🧑X&apos;s Expense</label>
-      <input type='text' disabled />
+      <label>🧑‍🤝‍🧑{selectedFriend.name}&apos;s Expense</label>
+      <input type='text' disabled value={paidByFriend} />
 
       <label>💸 Who&apos;s paying?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value='user'>You</option>
         <option value='friend'>{selectedFriend.name}</option>
       </select>
@@ -32,4 +53,5 @@ const SplitBillForm = ({ selectedFriend }) => {
 export default SplitBillForm
 SplitBillForm.propTypes = {
   selectedFriend: PropTypes.object,
+  onSplitBill: PropTypes.func,
 }
