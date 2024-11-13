05c9c7f6280f4193292837db5ff206203887ec20
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 18:37:23 -0500

    Pass movie data to Nav and Main components for improved state management

--- usePopcorn/src/App.jsx
+++ usePopcorn/src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react'
+import { tempMovieData } from './db'
 import Nav from './components/nav/nav.component'
 import Main from './components/main/main.component'
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData)
+
   return (
     <>
-      <Nav />
+      <Nav movies={movies} />
 
-      <Main />
+      <Main movies={movies} />
     </>
   )
 }
