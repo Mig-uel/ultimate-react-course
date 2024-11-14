0a5c5e63e4b49eabce49befc0d9c4dc346937af2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-13 21:16:25 -0500

    Enhance StarRating component with customizable color, size, and default rating options

--- usePopcorn/src/components/star-rating/star-rating.component.jsx
+++ usePopcorn/src/components/star-rating/star-rating.component.jsx
@@ -12,19 +12,14 @@ const starContainerStyle = {
   display: 'flex',
 }
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-}
+const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer',
+  }
 
-const starStyle = {
-  width: '48px',
-  height: '48px',
-  display: 'block',
-  cursor: 'pointer',
-}
-
-const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
   return (
     <span
       role='button'
@@ -37,8 +32,8 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
-          fill='#000'
-          stroke='#000'
+          fill={color}
+          stroke={color}
         >
           <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
         </svg>
@@ -47,7 +42,7 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke='#000'
+          stroke={color}
         >
           <path
             strokeLinecap='round'
@@ -61,14 +56,28 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
   )
 }
 
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(0)
+const StarRating = ({
+  maxRating = 5,
+  color = '#FCC419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+}) => {
+  const [rating, setRating] = useState(defaultRating)
   const [hoverRating, setHoverRating] = useState(0)
 
   const handleRating = (rating) => setRating(rating)
 
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  }
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, index) => (
           <Star
@@ -77,10 +86,16 @@ const StarRating = ({ maxRating = 5 }) => {
             full={hoverRating ? index + 1 <= hoverRating : index + 1 <= rating}
             onHoverIn={() => setHoverRating(index + 1)}
             onHoverOut={() => setHoverRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{hoverRating || rating || ''}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[hoverRating ? hoverRating - 1 : rating - 1]
+          : hoverRating || rating || ''}
+      </p>
     </div>
   )
 }
