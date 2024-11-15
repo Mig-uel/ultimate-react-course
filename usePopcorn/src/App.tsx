cb2aa88952247575a5aab53f171460defef9e054
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-14 19:01:37 -0500

    Refactor imports in App component to use path aliasing for cleaner code

--- usePopcorn/src/App.tsx
+++ usePopcorn/src/App.tsx
@@ -4,15 +4,15 @@ import { tempMovieData, tempWatchedData } from './db'
 import Box from './components/box.component'
 
 // NAV COMPONENTS
-import Nav from './components/nav/nav.component'
-import Search from './components/nav/search.component'
-import NumResults from './components/nav/num-results.component'
+import Nav from '@/components/nav/nav.component'
+import Search from '@/components/nav/search.component'
+import NumResults from '@/components/nav/num-results.component'
 
 // MAIN COMPONENTS
-import Main from './components/main/main.component'
-import ResultsMovieList from './components/main/results-movie-list.component'
-import WatchedSummary from './components/main/watched-summary.component'
-import WatchedMovieList from './components/main/watched-movie-list.component'
+import Main from '@/components/main/main.component'
+import ResultsMovieList from '@/components/main/results-movie-list.component'
+import WatchedSummary from '@/components/main/watched-summary.component'
+import WatchedMovieList from '@/components/main/watched-movie-list.component'
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData)
