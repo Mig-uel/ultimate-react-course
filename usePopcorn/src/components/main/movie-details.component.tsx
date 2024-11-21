ad16af110de20aa35d8c1062dd0ad2b2260889bc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-20 23:12:46 -0500

    Add loading state and loader component to movie details

--- usePopcorn/src/components/main/movie-details.component.tsx
+++ usePopcorn/src/components/main/movie-details.component.tsx
@@ -1,6 +1,7 @@
 import { ExtendedMovieData } from '@/types/types'
 import { useEffect, useState } from 'react'
 import StarRating from '../star-rating/star-rating.component'
+import Loader from '../loader.component'
 
 const OMDb_URI = `http://www.omdbapi.com/?apikey=${
   import.meta.env.VITE_OMDB_KEY
@@ -13,15 +14,18 @@ const MovieDetails = ({
   selectedId: string
   handleCloseSelectedMovie: () => void
 }) => {
+  const [isLoading, setIsLoading] = useState(false)
   const [selectedMovieDetails, setSelectedMovieDetails] =
     useState<ExtendedMovieData | null>(null)
 
   useEffect(() => {
     const getSelectedMovieDetails = async () => {
+      setIsLoading(true)
       const res = await fetch(`${OMDb_URI}&i=${selectedId}&plot=full`)
       const data = await res.json()
 
       setSelectedMovieDetails(data)
+      setIsLoading(false)
     }
 
     getSelectedMovieDetails()
@@ -29,41 +33,47 @@ const MovieDetails = ({
 
   return (
     <div className='details'>
-      <header>
-        <button className='btn-back' onClick={handleCloseSelectedMovie}>
-          &times;
-        </button>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className='btn-back' onClick={handleCloseSelectedMovie}>
+              &times;
+            </button>
 
-        <img
-          src={selectedMovieDetails?.Poster}
-          alt={`Poster of ${selectedMovieDetails?.Title}`}
-        />
+            <img
+              src={selectedMovieDetails?.Poster}
+              alt={`Poster of ${selectedMovieDetails?.Title}`}
+            />
 
-        <div className='details-overview'>
-          <h2>{selectedMovieDetails?.Title}</h2>
-          <p>
-            {selectedMovieDetails?.Released} &bull;{' '}
-            {selectedMovieDetails?.Runtime}
-          </p>
-          <p>{selectedMovieDetails?.Genre}</p>
-          <p>
-            <span>🌟</span> {selectedMovieDetails?.imdbRating} IMDb rating
-          </p>
-        </div>
-      </header>
+            <div className='details-overview'>
+              <h2>{selectedMovieDetails?.Title}</h2>
+              <p>
+                {selectedMovieDetails?.Released} &bull;{' '}
+                {selectedMovieDetails?.Runtime}
+              </p>
+              <p>{selectedMovieDetails?.Genre}</p>
+              <p>
+                <span>🌟</span> {selectedMovieDetails?.imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
 
-      <section>
-        <div className='rating'>
-          <StarRating maxRating={10} size={24} />
-        </div>
-        <p>
-          <em>{selectedMovieDetails?.Plot}</em>
-        </p>
+          <section>
+            <div className='rating'>
+              <StarRating maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{selectedMovieDetails?.Plot}</em>
+            </p>
 
-        <p>Starring {selectedMovieDetails?.Actors}</p>
+            <p>Starring {selectedMovieDetails?.Actors}</p>
 
-        <p>Directed by {selectedMovieDetails?.Director}</p>
-      </section>
+            <p>Directed by {selectedMovieDetails?.Director}</p>
+          </section>
+        </>
+      )}
     </div>
   )
 }
