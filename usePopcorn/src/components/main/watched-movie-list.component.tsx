0505472725527c554947a6b5cb948970934ab7fd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-21 01:46:15 -0500

    Pass handleDeleteWatchedMovie prop to WatchedMovieList for improved movie removal functionality

--- usePopcorn/src/components/main/watched-movie-list.component.tsx
+++ usePopcorn/src/components/main/watched-movie-list.component.tsx
@@ -1,12 +1,22 @@
 import { WatchedData } from '@/types/types'
 import WatchedMovie from './watched-movie.component'
 
-const WatchedMovieList = ({ watched }: { watched: WatchedData[] }) => {
+const WatchedMovieList = ({
+  watched,
+  handleDeleteWatchedMovie,
+}: {
+  watched: WatchedData[]
+  handleDeleteWatchedMovie: (id: string) => void
+}) => {
   return (
     <ul className='list'>
       {watched.map((movie) => (
         // WATCHED MOVIE
-        <WatchedMovie key={movie.imdbID} movie={movie} />
+        <WatchedMovie
+          key={movie.imdbID}
+          movie={movie}
+          handleDeleteWatchedMovie={handleDeleteWatchedMovie}
+        />
       ))}
     </ul>
   )
