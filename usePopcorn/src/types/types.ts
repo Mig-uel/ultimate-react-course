17435fa952a5ec1bd558969624af854c42d5ef37
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-20 22:58:22 -0500

    Add ExtendedMovieData type with additional movie properties for enhanced data handling

--- usePopcorn/src/types/types.ts
+++ usePopcorn/src/types/types.ts
@@ -14,3 +14,32 @@ export type WatchedData = {
   imdbRating: number
   userRating: number
 }
+
+type Rating = {
+  Source: string
+  Value: string
+}
+
+export type ExtendedMovieData = MovieData & {
+  Rated: string
+  Released: string
+  Runtime: string
+  Genre: string
+  Director: string
+  Writer: string
+  Actors: string
+  Plot: string
+  Language: string
+  Country: string
+  Awards: string
+  Ratings: Rating[]
+  Metascore: string
+  imdbRating: string
+  imdbVotes: string
+  Type: string
+  DVD: string
+  BoxOffice: string
+  Production: string
+  Website: string
+  Response: string
+}
