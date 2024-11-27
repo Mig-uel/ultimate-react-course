04a882b6d3db8af12b00926a42afcc961aef2fa9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 16:05:36 -0500

    Add onClick handler to Start button for dispatching 'start' action

--- react-quiz/src/components/start-screen.component.tsx
+++ react-quiz/src/components/start-screen.component.tsx
@@ -12,7 +12,12 @@ const StartScreen = ({
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numOfQuestions} questions to test your React mastery</h3>
 
-      <button className='btn btn-ui'>Let's Start!</button>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        Let's Start!
+      </button>
     </div>
   )
 }
