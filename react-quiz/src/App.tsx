4ba90855643c2d53c90ed8df59cbc147fe193a22
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 15:30:19 -0500

    Enhance reducer to handle data failure and update fetchQuestions to check response status

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -9,10 +9,12 @@ const initialState: State = {
   questions: [],
   status: 'loading',
 }
-const reducer = (state: State, { type, payload }: Action): State => {
+const reducer = (state: State, { type, payload }: Action<Question>): State => {
   switch (type) {
     case 'dataReceived':
       return { ...state, questions: payload!, status: 'ready' }
+    case 'dataFailed':
+      return { ...state, status: 'error' }
     default: {
       const never: never = type
       throw new Error(`INVALID ACTION TYPE: ${never}`)
@@ -24,20 +26,24 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    async function fetchQuestions<T extends Question>() {
+    async function fetchQuestions() {
       try {
         const res = await fetch('http://localhost:3001/questions')
-        const data = (await res.json()) as T[]
+        const data = (await res.json()) as Question[]
 
-        if (!data.length) throw new Error('Cannot fetch data')
+        if (!res.ok) throw new Error('Cannot fetch data')
 
         dispatch({ type: 'dataReceived', payload: data })
       } catch (error) {
-        console.log(error)
+        if (error instanceof Error) {
+          console.log(error.message)
+
+          dispatch({ type: 'dataFailed' })
+        }
       }
     }
 
-    fetchQuestions<Question>()
+    fetchQuestions<Question[]>()
   }, [])
 
   return (
