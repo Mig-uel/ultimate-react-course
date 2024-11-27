b1947f3053c506a1c0c0eb15b8d2ffe827bff9d4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 13:28:23 -0500

    Add Main component to display quiz questions in App

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,9 +1,15 @@
 import Header from './components/header.component'
+import Main from './components/main.component'
 
 function App() {
   return (
     <div className='app'>
       <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   )
 }
