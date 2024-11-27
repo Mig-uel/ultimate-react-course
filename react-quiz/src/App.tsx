110c7af568878e45ad01b6715a9e80feb32ba833
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 13:46:29 -0500

    Implement fetchQuestions function to retrieve quiz questions from the API

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from 'react'
 import Header from './components/header.component'
 import Main from './components/main.component'
 
+type Options = string[]
+type Question = {
+  question: string
+  options: Options[]
+  correctOption: number
+  points: number
+  id: string
+}
+
 function App() {
+
+  
+
+  useEffect(() => {
+    async function fetchQuestions<T = []>() {
+      try {
+        const res = await fetch('http://localhost:3001/questions')
+        const data = (await res.json()) as T
+
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchQuestions<Question[]>()
+  }, [])
+
   return (
     <div className='app'>
       <Header />
