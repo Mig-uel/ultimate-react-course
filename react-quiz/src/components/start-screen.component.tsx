0d9d658bda6c05caa5eaf7bd0c88c0547369b23f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 15:56:36 -0500

    Add StartScreen component for quiz introduction

---
+++ react-quiz/src/components/start-screen.component.tsx
@@ -0,0 +1,11 @@
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>X questions to test your React mastery</h3>
+
+      <button>Let's Start!</button>
+    </div>
+  )
+}
+export default StartScreen
