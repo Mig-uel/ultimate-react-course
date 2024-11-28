4a8d39ce9494a385d3e1f835e55defc3f4de802b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 13:58:50 -0500

    Rename import for FinalScreen component to reflect correct file name

--- react-quiz/src/components/index.ts
+++ react-quiz/src/components/index.ts
@@ -1,7 +1,7 @@
 export { default as ActiveQuestion } from './active-question.component'
 export { default as ErrorMessage } from './error-message.component'
 export { default as Header } from './header.component'
-export { default as FinalScreen } from './start-screen.component'
+export { default as FinalScreen } from './final-screen.component'
 export { default as Loader } from './loader.component'
 export { default as Main } from './main.component'
 export { default as NextButton } from './next-button.component'
