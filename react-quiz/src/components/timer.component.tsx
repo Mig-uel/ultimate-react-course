1d1a7cca2f4d68c54048061ff844c698d383247f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 17:29:04 -0500

    Format Timer component to display time in MM:SS format

--- react-quiz/src/components/timer.component.tsx
+++ react-quiz/src/components/timer.component.tsx
@@ -8,6 +8,9 @@ const Timer = ({
   dispatch: React.Dispatch<Action>
   secondsRemaining: number | null
 }) => {
+  const min = Math.floor(secondsRemaining! / 60)
+  const sec = secondsRemaining! % 60
+
   useEffect(() => {
     const id = setInterval(() => {
       dispatch({ type: 'tick' })
@@ -16,6 +19,12 @@ const Timer = ({
     return () => clearInterval(id)
   }, [dispatch])
 
-  return <div className='timer'>{secondsRemaining}</div>
+  return (
+    <div className='timer'>
+      {min < 10 && 0}
+      {min}:{sec < 10 && 0}
+      {sec}
+    </div>
+  )
 }
 export default Timer
