86182b136558ffaad67ee80ca5810a20af5ca7f7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 13:02:14 -0500

    Refactor App component to use quizReducer for state management

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -8,49 +8,8 @@ import StartScreen from './components/start-screen.component'
 import ActiveQuestion from './components/active-question.component'
 import NextButton from './components/next-button.component'
 
-import type { Action, Question, State } from './types'
-
-const initialState: State = {
-  questions: [],
-  status: 'loading',
-  error: null,
-  index: 0,
-  answer: null,
-  points: 0,
-}
-const reducer = (state: State, { type, payload }: Action): State => {
-  switch (type) {
-    case 'dataReceived':
-      return {
-        ...state,
-        questions: payload as Question[],
-        status: 'ready',
-        error: null,
-      }
-    case 'dataFailed':
-      return { ...state, status: 'error', error: payload as string }
-    case 'start':
-      return { ...state, status: 'active' }
-    case 'newAnswer': {
-      const currentQuestion = state.questions[state.index]
-
-      return {
-        ...state,
-        answer: payload as number,
-        points:
-          payload === currentQuestion.correctOption
-            ? state.points + currentQuestion.points
-            : state.points,
-      }
-    }
-    case 'nextQuestion':
-      return { ...state, index: state.index + 1, answer: null }
-    default: {
-      const never: never = type
-      throw new Error(`INVALID ACTION TYPE: ${never}`)
-    }
-  }
-}
+import { initialState, reducer } from './reducers/quizReducer'
+import type { Question } from './types'
 
 function App() {
   const [{ error, questions, status, index, answer }, dispatch] = useReducer(
