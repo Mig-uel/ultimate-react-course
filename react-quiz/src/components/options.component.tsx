0896fdbbf338217d133923078652d9b16deadd9d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 23:21:59 -0500

    Enhance Options component to handle user answers and dispatch newAnswer action

--- react-quiz/src/components/options.component.tsx
+++ react-quiz/src/components/options.component.tsx
@@ -1,10 +1,34 @@
-import type { Question } from '../types'
+import type { Action, Question } from '../types'
 
-const Options = ({ question }: { question: Question }) => {
+const Options = ({
+  question,
+  dispatch,
+  answer,
+}: {
+  question: Question
+  dispatch: React.Dispatch<Action>
+  answer: number | null
+}) => {
+  console.log(question)
+  console.log(answer)
+
+  const isAnswered = answer !== null
+
   return (
     <div className='options'>
-      {question.options.map((option) => (
-        <button key={option} className='btn btn-option'>
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            isAnswered
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+          disabled={isAnswered}
+        >
           {option}
         </button>
       ))}
