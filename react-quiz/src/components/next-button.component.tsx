e140a1b326faeae4a022a9caf2d68cf37e8bd64a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 14:05:50 -0500

    Add conditional rendering for NextButton to handle quiz progression and completion

--- react-quiz/src/components/next-button.component.tsx
+++ react-quiz/src/components/next-button.component.tsx
@@ -3,19 +3,34 @@ import type { Action } from '../types'
 const NextButton = ({
   dispatch,
   answer,
+  index,
+  numOfQuestions,
 }: {
   dispatch: React.Dispatch<Action>
   answer: number | null
+  index: number
+  numOfQuestions: number
 }) => {
   if (answer === null) return null
 
-  return (
-    <button
-      className='btn btn-ui'
-      onClick={() => dispatch({ type: 'nextQuestion' })}
-    >
-      Next
-    </button>
-  )
+  if (index < numOfQuestions - 1)
+    return (
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'nextQuestion' })}
+      >
+        Next
+      </button>
+    )
+
+  if (index === numOfQuestions - 1)
+    return (
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'finish' })}
+      >
+        Finish
+      </button>
+    )
 }
 export default NextButton
