fdb413b63d569d35442ad9c3e163d3638c1d462a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 13:02:12 -0500

    Add quizReducer to manage quiz state and actions

---
+++ react-quiz/src/reducers/quizReducer.tsx
@@ -0,0 +1,44 @@
+import type { Action, Question, State } from '../types'
+
+export const initialState: State = {
+  questions: [],
+  status: 'loading',
+  error: null,
+  index: 0,
+  answer: null,
+  points: 0,
+}
+
+export const reducer = (state: State, { type, payload }: Action): State => {
+  switch (type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: payload as Question[],
+        status: 'ready',
+        error: null,
+      }
+    case 'dataFailed':
+      return { ...state, status: 'error', error: payload as string }
+    case 'start':
+      return { ...state, status: 'active' }
+    case 'newAnswer': {
+      const currentQuestion = state.questions[state.index]
+
+      return {
+        ...state,
+        answer: payload as number,
+        points:
+          payload === currentQuestion.correctOption
+            ? state.points + currentQuestion.points
+            : state.points,
+      }
+    }
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null }
+    default: {
+      const never: never = type
+      throw new Error(`INVALID ACTION TYPE: ${never}`)
+    }
+  }
+}
