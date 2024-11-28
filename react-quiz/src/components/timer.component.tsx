1c2360a577bf4a4f356169b9828b68f9420fca5e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 17:21:51 -0500

    Implement Timer component with countdown functionality and dispatch 'tick' action

--- react-quiz/src/components/timer.component.tsx
+++ react-quiz/src/components/timer.component.tsx
@@ -1,4 +1,21 @@
-const Timer = () => {
-  return <div>Timer</div>
+import { useEffect } from 'react'
+import type { Action } from '../types'
+
+const Timer = ({
+  dispatch,
+  secondsRemaining,
+}: {
+  dispatch: React.Dispatch<Action>
+  secondsRemaining: number
+}) => {
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: 'tick' })
+    }, 1000)
+
+    return () => clearInterval(id)
+  }, [dispatch])
+
+  return <div className='timer'>{secondsRemaining}</div>
 }
 export default Timer
