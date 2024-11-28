0d895f28c08b3d28c54e9506679a82ec0b593959
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 23:22:02 -0500

    Update ActiveQuestion component to accept dispatch and answer props for improved state management

--- react-quiz/src/components/active-question.component.tsx
+++ react-quiz/src/components/active-question.component.tsx
@@ -1,15 +1,21 @@
 import Options from './options.component'
 
-import type { Question } from '../types'
+import type { Action, Question } from '../types'
 
-const ActiveQuestion = ({ question }: { question: Question }) => {
-  console.log(question)
-
+const ActiveQuestion = ({
+  question,
+  dispatch,
+  answer,
+}: {
+  question: Question
+  dispatch: React.Dispatch<Action>
+  answer: number | null
+}) => {
   return (
     <div>
       <h4>{question.question}</h4>
 
-      <Options question={question} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   )
 }
