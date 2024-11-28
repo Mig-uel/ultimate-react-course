a2858cc08433fd1db2079cb7b72bc1e2ddf6debc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-28 14:10:04 -0500

    Add emoji feedback to FinalScreen based on quiz score percentage

--- react-quiz/src/components/final-screen.component.tsx
+++ react-quiz/src/components/final-screen.component.tsx
@@ -7,8 +7,17 @@ const FinalScreen = ({
 }) => {
   const percentage = (points / maxPoints) * 100
 
+  let emoji: string = ''
+
+  if (percentage === 100) emoji = '🥇'
+  if (percentage >= 80 && percentage < 100) emoji = '🥈'
+  if (percentage >= 50 && percentage < 80) emoji = '🥉'
+  if (percentage >= 50 && percentage < 80) emoji = '🤔'
+  if (percentage === 0) emoji = '👎'
+
   return (
     <p className='result'>
+      <span>{emoji}</span>
       You scored:<strong>{points}</strong> out of {maxPoints} (
       {Math.ceil(percentage)}%)
     </p>
