d04c0f5f4a88d822877a37f4d842c21e30092092
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-29 20:36:34 -0500

    Add index.ts to export page components for easier imports

---
+++ worldwise/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './home.page'
+export { default as PageNotFound } from './page-not-found'
+export { default as Pricing } from './pricing.page'
+export { default as Product } from './product.page'
