868f46d07844a9036500bf4f6b61a66cddf6ffd3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-29 20:21:37 -0500

    Add comments to App.tsx explaining Single Page Application behavior and routing

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -3,3 +3,10 @@ function App() {
 }
 
 export default App
+
+/* 
+  Single Page Application (SPA) are executed entirely on the client (browsers)
+  Routes: different URLs correspond to different views (components)
+  JavaScript is used to update the page (DOM)
+  The page is never reloaded
+*/
