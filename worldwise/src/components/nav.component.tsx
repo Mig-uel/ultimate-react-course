275baf111e555e4088e58a3244b0d1d1626b009d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-29 21:40:33 -0500

    Replace Link with NavLink in Nav component for improved navigation functionality

--- worldwise/src/components/nav.component.tsx
+++ worldwise/src/components/nav.component.tsx
@@ -1,17 +1,17 @@
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 
 const Nav = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <NavLink to='/'>Home</NavLink>
         </li>
         <li>
-          <Link to='/pricing'>Pricing</Link>
+          <NavLink to='/pricing'>Pricing</NavLink>
         </li>
         <li>
-          <Link to='/product'>Product</Link>
+          <NavLink to='/product'>Product</NavLink>
         </li>
       </ul>
     </nav>
