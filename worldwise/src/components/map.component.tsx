ec5226dc0cbbd6aafbbfd265c7b30f6bae2b593b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 14:40:50 -0500

    Update Map component to display latitude and longitude from search parameters

--- worldwise/src/components/map.component.tsx
+++ worldwise/src/components/map.component.tsx
@@ -1,6 +1,19 @@
+import { useSearchParams } from 'react-router'
 import styles from '../styles/map.module.css'
 
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const lat = searchParams.get('lat')!
+  const lng = searchParams.get('lng')!
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+
+      <p>{lat}</p>
+      <p>{lng}</p>
+    </div>
+  )
 }
 export default Map
