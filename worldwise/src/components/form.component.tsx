53159993ad706d72648e15cda33050a0957d4c10
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 15:20:12 -0500

    Add Form component with input fields for city name, date, and notes

---
+++ worldwise/src/components/form.component.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Button } from '../components'
+import styles from '../styles/form.module.css'
+import { useNavigate } from 'react-router'
+
+const Form = () => {
+  const navigate = useNavigate()
+  const [cityName, setCityName] = useState('')
+  // const [country, setCountry] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [notes, setNotes] = useState('')
+
+  return (
+    <form className={styles.form}>
+      <div className={styles.row}>
+        <label htmlFor='cityName'>City name</label>
+        <input
+          id='cityName'
+          onChange={(e) => setCityName(e.target.value)}
+          value={cityName}
+        />
+        {/* <span className={styles.flag}>{emoji}</span> */}
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor='date'>When did you go to {cityName}?</label>
+        <input
+          id='date'
+          onChange={(e) => setDate(new Date(e.target.value))}
+          value={date.toLocaleDateString()}
+        />
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor='notes'>Notes about your trip to {cityName}</label>
+        <textarea
+          id='notes'
+          onChange={(e) => setNotes(e.target.value)}
+          value={notes}
+        />
+      </div>
+
+      <div className={styles.buttons}>
+        <Button onClick={() => navigate('')} type='primary'>
+          Add
+        </Button>
+        <Button onClick={() => navigate(-1)} type='back'>
+          &larr; Back
+        </Button>
+      </div>
+    </form>
+  )
+}
+export default Form
