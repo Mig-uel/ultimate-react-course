695fc7984b40d0a596726d4c3d23f5657c517d16
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 12:01:22 -0500

    Remove Nav component from MainLayout for simplified structure

--- worldwise/src/pages/main-layout.page.tsx
+++ worldwise/src/pages/main-layout.page.tsx
@@ -1,13 +1,6 @@
 import { Outlet } from 'react-router'
-import { Nav } from '../components'
 
 const MainLayout = () => {
-  return (
-    <div>
-      <Nav />
-
-      <Outlet />
-    </div>
-  )
+  return <Outlet />
 }
 export default MainLayout
