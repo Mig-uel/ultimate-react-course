f90bbcab35b77307e5ef70d8f2255b5d6af17da8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 12:23:32 -0500

    Enhance AppNav component with navigation links for Cities and Countries

--- worldwise/src/components/app-nav.component.tsx
+++ worldwise/src/components/app-nav.component.tsx
@@ -1,6 +1,18 @@
+import { NavLink } from 'react-router'
 import styles from '../styles/app-nav.module.css'
 
 const AppNav = () => {
-  return <nav className={styles.nav}>AppNav</nav>
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to='cities'>Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to='countries'>Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 export default AppNav
