6b9e26f2cf88dd2a4f55fc8b949c1ae6672d1e1c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 12:44:34 -0500

    Fetch cities data from API and manage loading state in App component

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { CitiesList } from './components'
 import { Home, Login, PageNotFound, Pricing, Product } from './pages'
 import MainLayout from './pages/main-layout.page'
 import AppLayout from './pages/app-layout.component'
-import { CitiesList } from './components'
+import type { City } from './types'
+
+const BASE_URL = 'http://localhost:3001/'
 
 function App() {
+  const [cities, setCities] = useState<City[] | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+
+        const res = await fetch(BASE_URL + 'cities')
+
+        const data = await res.json()
+
+        setCities(data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
