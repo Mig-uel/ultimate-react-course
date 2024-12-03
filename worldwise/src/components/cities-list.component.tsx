92da9904edcc8657617a6c99f968e2e8324bb8c4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 12:30:28 -0500

    Add CitiesList component and export from index

---
+++ worldwise/src/components/cities-list.component.tsx
@@ -0,0 +1,6 @@
+import styles from '../styles/cities-list.module.css'
+
+const CitiesList = () => {
+  return <ul className={styles.cityList}>CitiesList</ul>
+}
+export default CitiesList
--- worldwise/src/components/index.ts
+++ worldwise/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as AppNav } from './app-nav.component'
+export { default as CitiesList } from './cities-list.component'
 export { default as Logo } from './logo.component'
 export { default as Map } from './map.component'
 export { default as Nav } from './nav.component'
