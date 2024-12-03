0843bbab383d8058874492a008fb84a16ce52c3f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-03 14:40:42 -0500

    Update City component to include position parameters in link

--- worldwise/src/components/city-item.component.tsx
+++ worldwise/src/components/city-item.component.tsx
@@ -10,10 +10,13 @@ const formatDate = (date: string) =>
     weekday: 'long',
   }).format(new Date(date))
 
-const City = ({ cityName, date, emoji, id }: types.CityItem) => {
+const City = ({ cityName, date, emoji, id, position }: types.CityItem) => {
   return (
     <li>
-      <Link to={id} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date || '')})</time>
