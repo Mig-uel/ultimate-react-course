3b5e8015323c7ece92f9886e11917b81f973f036
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 09:31:36 -0500

    Wrap application in CitiesProvider for improved context management

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -1,66 +1,35 @@
-import { useEffect, useState } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import { CitiesProvider } from './context/CitiesContext'
 import { CitiesList, City, CountriesList, Form } from './components'
 import { Home, Login, PageNotFound, Pricing, Product } from './pages'
 import MainLayout from './pages/main-layout.page'
 import AppLayout from './pages/app-layout.component'
-import type { CityItem } from './types'
-
-const BASE_URL = 'http://localhost:3001/'
 
 function App() {
-  const [cities, setCities] = useState<CityItem[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true)
-
-        const res = await fetch(BASE_URL + 'cities')
-
-        const data = await res.json()
-
-        setCities(data)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    fetchCities()
-  }, [])
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='product' element={<Product />} />
-          <Route path='pricing' element={<Pricing />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Route>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='product' element={<Product />} />
+            <Route path='pricing' element={<Pricing />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Route>
 
-        {/* APP ROUTES */}
-        <Route element={<AppLayout />} path='/app'>
-          
-          <Route index element={<Navigate to='cities' replace />} />
-          <Route
-            path='cities'
-            element={<CitiesList cities={cities} isLoading={isLoading} />}
-          />
+          {/* APP ROUTES */}
+          <Route element={<AppLayout />} path='/app'>
+            <Route index element={<Navigate to='cities' replace />} />
+            <Route path='cities' element={<CitiesList />} />
 
-          <Route path='cities/:id' element={<City />} />
-          <Route
-            path='countries'
-            element={<CountriesList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path='form' element={<Form />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            <Route path='cities/:id' element={<City />} />
+            <Route path='countries' element={<CountriesList />} />
+            <Route path='form' element={<Form />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   )
 }
 
