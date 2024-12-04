8eaf040b4bda03a92ec91da7ed39064fc801d65c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 04:19:49 -0500

    Refactor Main component to remove props and simplify structure

--- atomic-blog/src/components/main.tsx
+++ atomic-blog/src/components/main.tsx
@@ -1,18 +1,11 @@
 import Posts from './posts'
 import FormAddPost from './add-post-form'
-import type * as types from '../types'
 
-function Main({
-  posts,
-  onAddPost,
-}: {
-  posts: types.Post[]
-  onAddPost: (post: types.Post) => void
-}) {
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts />
     </main>
   )
 }
