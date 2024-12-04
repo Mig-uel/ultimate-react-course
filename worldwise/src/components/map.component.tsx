d64a6b6de644b7ba1f0c07acb462688005a7d8bd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 11:40:29 -0500

    Implement map component with Leaflet integration and dynamic marker positioning

--- worldwise/src/components/map.component.tsx
+++ worldwise/src/components/map.component.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import styles from '../styles/map.module.css'
 
 const Map = () => {
   const navigate = useNavigate()
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams] = useSearchParams()
+
+  const [mapPosition, setMapPosition] = useState<[number, number]>([40, 0])
 
   const lat = searchParams.get('lat')!
   const lng = searchParams.get('lng')!
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-
-      <p>{lat}</p>
-      <p>{lng}</p>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
