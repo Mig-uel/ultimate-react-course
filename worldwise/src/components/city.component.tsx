2d659dc5ed12aa522b58703269a9b7e2da37d564
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 10:18:35 -0500

    Refactor City component to use CitiesContext for state management and enhance UI

--- worldwise/src/components/city.component.tsx
+++ worldwise/src/components/city.component.tsx
@@ -1,60 +1,60 @@
-import { useParams, useSearchParams } from 'react-router'
-// import styles from '../styles/city.module.css'
+import { useParams } from 'react-router'
+import { useCitiesContext } from '../context/CitiesContext'
+import { useEffect } from 'react'
+import styles from '../styles/city.module.css'
+import Spinner from './spinner.component'
 
-// const formatDate = (date: string) =>
-//   new Intl.DateTimeFormat('en', {
-//     day: 'numeric',
-//     month: 'long',
-//     year: 'numeric',
-//     weekday: 'long',
-//   }).format(new Date(date))
+const formatDate = (date: string) =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date))
 
 const City = () => {
   const { id } = useParams()
-  const [searchParams, setSearchParams] = useSearchParams()
+  const { currentCity, getCity, isLoading } = useCitiesContext()
 
-  const lat = searchParams.get('lat')!
-  const lng = searchParams.get('lng')!
+  useEffect(() => {
+    getCity(id as string)
+  }, [id])
+
+  if (isLoading) return <Spinner />
+  if (!currentCity) return <h1>Something went wrong...</h1>
 
   return (
-    <div>
-      <h1>City: {id}</h1>
-      <p>{lat}</p>
-      <p>{lng}</p>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{currentCity?.emoji}</span> {currentCity?.cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {currentCity?.cityName} on</h6>
+        <p>{formatDate(currentCity.date)}</p>
+      </div>
+
+      {currentCity?.notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{currentCity.notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${currentCity?.cityName}`}
+          target='_blank'
+          rel='noreferrer'
+        >
+          Check out {currentCity?.cityName} on Wikipedia &rarr;
+        </a>
+      </div>
     </div>
   )
-  // return (
-  //   <div className={styles.city}>
-  //     <div className={styles.row}>
-  //       <h6>City name</h6>
-  //       <h3>
-  //         <span>{emoji}</span> {cityName}
-  //       </h3>
-  //     </div>
-
-  //     <div className={styles.row}>
-  //       <h6>You went to {cityName} on</h6>
-  //       <p>{formatDate(date || '')}</p>
-  //     </div>
-
-  //     {notes && (
-  //       <div className={styles.row}>
-  //         <h6>Your notes</h6>
-  //         <p>{notes}</p>
-  //       </div>
-  //     )}
-
-  //     <div className={styles.row}>
-  //       <h6>Learn more</h6>
-  //       <a
-  //         href={`https://en.wikipedia.org/wiki/${cityName}`}
-  //         target='_blank'
-  //         rel='noreferrer'
-  //       >
-  //         Check out {cityName} on Wikipedia &rarr;
-  //       </a>
-  //     </div>
-  //   </div>
-  // )
 }
 export default City
