8a1077649cc007825f7c5cb60740f1e1401f456a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 04:19:45 -0500

    Refactor Archive component to consume PostContext and remove unnecessary props

--- atomic-blog/src/components/archive.tsx
+++ atomic-blog/src/components/archive.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import PostContext from '../context/PostContext'
 import { createRandomPost } from '../utils/createRandomPost'
-import type * as types from '../types'
 
-function Archive({ onAddPost }: { onAddPost: (post: types.Post) => void }) {
+function Archive() {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   )
-
   const [showArchive, setShowArchive] = useState(false)
+  const { onAddPost } = useContext(PostContext)
 
   return (
     <aside>
