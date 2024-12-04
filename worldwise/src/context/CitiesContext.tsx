e6f63d7e4bd74920ea8bb097f103dfed183971b3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 10:18:29 -0500

    Enhance CitiesContext to include currentCity state and getCity function for fetching individual city data

--- worldwise/src/context/CitiesContext.tsx
+++ worldwise/src/context/CitiesContext.tsx
@@ -1,28 +1,26 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import type { CitiesContextState, CityItem } from '../types'
 
-const BASE_URL = 'http://localhost:3001/'
+const BASE_URL = 'http://localhost:3001'
 
 const CitiesContext = createContext<CitiesContextState>({
   cities: [],
   isLoading: false,
+  currentCity: null,
+  getCity: async () => {},
 })
 
 const CitiesProvider = ({ children }: { children: React.ReactNode }) => {
   const [cities, setCities] = useState<CityItem[]>([])
   const [isLoading, setIsLoading] = useState(false)
-
-  const value = {
-    cities,
-    isLoading,
-  }
+  const [currentCity, setCurrentCity] = useState<CityItem | null>(null)
 
   useEffect(() => {
     async function fetchCities() {
       try {
         setIsLoading(true)
 
-        const res = await fetch(BASE_URL + 'cities')
+        const res = await fetch(BASE_URL + '/cities')
 
         const data = await res.json()
 
@@ -36,6 +34,29 @@ const CitiesProvider = ({ children }: { children: React.ReactNode }) => {
 
     fetchCities()
   }, [])
+
+  const getCity = async (id: string) => {
+    try {
+      setIsLoading(true)
+
+      const res = await fetch(`${BASE_URL}/cities/${id}`)
+
+      const data = await res.json()
+
+      setCurrentCity(data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const value = {
+    cities,
+    isLoading,
+    currentCity,
+    getCity,
+  }
 
   return (
     <CitiesContext.Provider value={value}>{children}</CitiesContext.Provider>
