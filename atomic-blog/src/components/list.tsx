91bc8f74c0769c0adcd87bc9da154e5018e1ae9e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 03:43:57 -0500

    Add List component for displaying blog posts

---
+++ atomic-blog/src/components/list.tsx
@@ -0,0 +1,16 @@
+import type * as types from '../types'
+
+function List({ posts }: { posts: types.Post[] }) {
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default List
