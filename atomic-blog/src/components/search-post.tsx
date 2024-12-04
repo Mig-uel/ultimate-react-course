3e6b9524a6c0b43e1061e7caa835678d38fc8cdf
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-04 07:44:34 -0500

    Refactor SearchPosts component to use named import for PostContext

--- atomic-blog/src/components/search-post.tsx
+++ atomic-blog/src/components/search-post.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import PostContext from '../context/PostContext'
+import { PostContext } from '../context/PostContext'
 
 function SearchPosts() {
   const { searchQuery, setSearchQuery } = useContext(PostContext)
