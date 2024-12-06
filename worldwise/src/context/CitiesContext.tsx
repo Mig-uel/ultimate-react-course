81e823a5e29588a3447dc454fc62b090e03275ee
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-05 19:35:40 -0500

    Refactor CitiesContext to use useReducer for state management and implement error handling

--- worldwise/src/context/CitiesContext.tsx
+++ worldwise/src/context/CitiesContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import type { CitiesContextState, CityItem } from '../types'
+import { createContext, useContext, useEffect, useReducer } from 'react'
+import type { Action, CitiesContextState, CityItem, State } from '../types'
 
 const BASE_URL = 'http://localhost:3001'
 
@@ -10,27 +10,60 @@ const CitiesContext = createContext<CitiesContextState>({
   getCity: async () => {},
   addCity: async () => {},
   deleteCity: async () => {},
+  error: null,
 })
 
+const initialState: State = {
+  cities: [],
+  isLoading: false,
+  currentCity: null,
+  error: null,
+}
+const reducer = (state: State, { payload, type }: Action): State => {
+  switch (type) {
+    case 'cities/loaded':
+      return { ...state, isLoading: false, cities: payload as CityItem[] }
+    case 'cities/created':
+      return { ...state }
+    case 'cities/deleted':
+      return { ...state }
+    case 'loading':
+      return { ...state, isLoading: true }
+    case 'rejected':
+      return { ...state, isLoading: false, error: payload as string }
+    default: {
+      const never: never = type
+      throw new Error(`Type '${never}' is an invalid reducer action type`)
+    }
+  }
+}
+
 const CitiesProvider = ({ children }: { children: React.ReactNode }) => {
-  const [cities, setCities] = useState<CityItem[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [currentCity, setCurrentCity] = useState<CityItem | null>(null)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { cities, currentCity, isLoading } = state
 
   useEffect(() => {
     async function fetchCities() {
-      try {
-        setIsLoading(true)
+      // set loading to true
+      dispatch({ type: 'loading' })
 
+      try {
         const res = await fetch(BASE_URL + '/cities')
 
         const data = await res.json()
 
-        setCities(data)
+        // set cities
+        dispatch({ type: 'cities/loaded', payload: data })
       } catch (error) {
         console.log(error)
-      } finally {
-        setIsLoading(false)
+
+        if (error instanceof Error)
+          dispatch({ type: 'rejected', payload: error.message })
+        else
+          dispatch({
+            type: 'rejected',
+            payload: 'There was an error loading the data...',
+          })
       }
     }
 
