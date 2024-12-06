a072f3a82f36632b88f035fb28f3e109d4c2aa06
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-06 08:37:19 -0500

    Enhance PageNotFound component with a user-friendly message and navigation button

--- worldwise/src/pages/page-not-found.tsx
+++ worldwise/src/pages/page-not-found.tsx
@@ -1,6 +1,18 @@
+import { Link } from 'react-router'
+import { Button } from '../components'
+import styles from '../styles/page-not-found.module.css'
+
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className={styles.pnf}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>Oops, page not found!</h1>
+
+        <Link to='/'>
+          <Button>Return Home</Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 export default PageNotFound
