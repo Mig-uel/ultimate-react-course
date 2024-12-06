cde6042f5a94b44026cc31cd01c0aef8bec35f7a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-06 07:03:15 -0500

    Add authentication check in AppLayout and render User component

--- worldwise/src/pages/app-layout.component.tsx
+++ worldwise/src/pages/app-layout.component.tsx
@@ -1,9 +1,20 @@
-import { Map, Sidebar } from '../components'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { useAuthContext } from '../context/AuthContext'
+import { Map, Sidebar, User } from '../components'
 import styles from '../styles/app-layout.module.css'
 
 const AppLayout = () => {
+  const navigate = useNavigate()
+  const { isAuthenticated } = useAuthContext()
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate('/login')
+  }, [isAuthenticated, navigate])
+
   return (
     <div className={styles.app}>
+      <User />
       <Sidebar />
       <Map />
     </div>
