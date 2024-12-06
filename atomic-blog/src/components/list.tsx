211458cce0cfda6c3d3b92ff658efb869cd99514
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-06 13:26:09 -0500

    Add Test component to List for enhanced functionality

--- atomic-blog/src/components/list.tsx
+++ atomic-blog/src/components/list.tsx
@@ -1,15 +1,20 @@
+import Test from '../test'
 import type * as types from '../types'
 
 function List({ posts }: { posts: types.Post[] }) {
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      <Test />
+    </>
   )
 }
 
