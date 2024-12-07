71a6e4433b388c413015a5a2096169ccc9a12a71
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-07 16:16:01 -0500

    Add SpinnerFull component and corresponding styles for full-screen loading indicator

---
+++ worldwise/src/components/spinner-full.component.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from '.'
+import styles from '../styles/spinner-full.module.css'
+
+const SpinnerFull = () => {
+  return (
+    <div className={styles.spinnerFull}>
+      <Spinner />
+    </div>
+  )
+}
+export default SpinnerFull
