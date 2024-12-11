b5e86e50cfa316c0008207e8762cd359e01476e2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-10 22:22:20 -0500

    Import store directly in main.tsx for cleaner code structure

--- redux-intro/src/main.tsx
+++ redux-intro/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import './store/store.ts'
+import store from './store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
