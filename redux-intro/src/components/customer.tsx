b81777a4f72b2fd1a43c022f11b0daa9af8f690a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-10 23:12:23 -0500

    Add exclamation mark to welcome message in Customer component

--- redux-intro/src/components/customer.tsx
+++ redux-intro/src/components/customer.tsx
@@ -3,6 +3,6 @@ import { useAppSelector } from '../hooks'
 const Customer = () => {
   const name = useAppSelector((store) => store.customer?.full_name)
 
-  return <h2>Welcome, {name}</h2>
+  return <h2>Welcome, {name}!</h2>
 }
 export default Customer
