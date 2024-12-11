226a6dcc9f6d9c7f0b0d47c643ee818c68db3f47
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-10 22:22:32 -0500

    Add account action creators for deposit, withdraw, loan requests, and payments

---
+++ redux-intro/src/features/account/accountActions.ts
@@ -0,0 +1,29 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+/* ACCOUNT ACTION CREATORS */
+export const deposit = (
+  payload: number
+): PayloadAction<number, 'account/deposit'> => ({
+  payload,
+  type: 'account/deposit',
+})
+
+export const withdraw = (
+  payload: number
+): PayloadAction<number, 'account/withdraw'> => ({
+  payload,
+  type: 'account/withdraw',
+})
+
+export const request_loan = (payload: {
+  amount: number
+  purpose: string
+}): PayloadAction<
+  { amount: number; purpose: string },
+  'account/request_loan'
+> => ({ payload, type: 'account/request_loan' })
+
+export const pay_loan = (): PayloadAction<null, 'account/pay_loan'> => ({
+  payload: null,
+  type: 'account/pay_loan',
+})
