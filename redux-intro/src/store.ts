10fedae139a46c9afe000b32699a6112298c0ae2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-11 11:08:54 -0500

    Refactor store setup to use configureStore from Redux Toolkit for improved performance and simplicity

--- redux-intro/src/store.ts
+++ redux-intro/src/store.ts
@@ -1,24 +1,13 @@
-import { thunk } from 'redux-thunk'
-import {
-  applyMiddleware,
-  combineReducers,
-  legacy_createStore as createStore,
-} from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { configureStore } from '@reduxjs/toolkit'
 import { accountReducer } from './features/account/accountSlice'
 import { customerReducer } from './features/customer/customerSlice'
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 })
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-)
-
-export default store
 
 // GET THE TYPE OF OUR STORE VARIABLE
 export type AppStore = typeof store
@@ -28,3 +17,5 @@ export type RootState = ReturnType<AppStore['getState']>
 
 // INFERRED TYPES
 export type AppDispatch = AppStore['dispatch']
+
+export default store
