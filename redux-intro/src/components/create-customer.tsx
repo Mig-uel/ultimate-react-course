0065d26427827f3a41da9062a93735780f82690d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-10 23:12:19 -0500

    Implement customer creation functionality with form validation and dispatch action

--- redux-intro/src/components/create-customer.tsx
+++ redux-intro/src/components/create-customer.tsx
@@ -1,27 +1,41 @@
 import { useState } from 'react'
+import { useAppDispatch } from '../hooks'
+import { createCustomer } from '../features/customer/customerActions'
 
 function CreateCustomer() {
+  const dispatch = useAppDispatch()
   const [fullName, setFullName] = useState('')
-  const [nationalId, setNationalId] = useState('')
+  const [nationalID, setNationalID] = useState('')
 
-  function handleClick() {}
+  function handleClick() {
+    if (!fullName || !nationalID) return
+
+    const customer = {
+      full_name: fullName,
+      nationalID,
+    }
+
+    dispatch(createCustomer(customer))
+  }
 
   return (
     <div>
-      <h2>Create new customer</h2>
+      <h2>Create New customer</h2>
       <div className='inputs'>
         <div>
-          <label>Customer full name</label>
+          <label htmlFor='fullName'>Customer Full Name</label>
           <input
+            id='fullName'
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
           />
         </div>
         <div>
-          <label>National ID</label>
+          <label htmlFor='nationalID'>National ID</label>
           <input
-            value={nationalId}
-            onChange={(e) => setNationalId(e.target.value)}
+            id='nationalID'
+            value={nationalID}
+            onChange={(e) => setNationalID(e.target.value)}
           />
         </div>
         <button onClick={handleClick}>Create new customer</button>
