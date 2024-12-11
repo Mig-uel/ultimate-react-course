b3149a8341cc0182b0e29e5aff299a579f0495e3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-11 11:32:47 -0500

    Add account reducer implementation with initial state and action handlers

---
+++ redux-intro/src/features/account/accountSlice_v1.ts
@@ -0,0 +1,58 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { AccountDispatchTypes, AccountState } from '../../types'
+
+const initialStateAccount: AccountState = {
+  balance: 0,
+  loan: 0,
+  loan_purpose: '',
+  isLoading: false,
+}
+
+export function accountReducer(
+  state = initialStateAccount,
+  action: PayloadAction<
+    number | { amount: number; purpose: string } | null,
+    AccountDispatchTypes
+  >
+) {
+  switch (action.type) {
+    case 'account/deposit':
+      if (typeof action.payload === 'number')
+        return {
+          ...state,
+          balance: state.balance + action.payload,
+          isLoading: false,
+        }
+      break
+    case 'account/withdraw':
+      if (typeof action.payload === 'number')
+        return { ...state, balance: state.balance - action.payload! }
+      break
+    case 'account/request_loan': {
+      if (state.loan > 0) return state
+
+      if (action.payload !== null && typeof action.payload === 'object')
+        return {
+          ...state,
+          loan: action.payload.amount,
+          loan_purpose: action.payload.purpose,
+          balance: state.balance + action.payload.amount,
+        }
+      break
+    }
+    case 'account/pay_loan': {
+      return {
+        ...state,
+        loan: 0,
+        loan_purpose: '',
+        balance: state.balance - state.loan,
+      }
+    }
+    case 'account/converting': {
+      return { ...state, isLoading: true }
+    }
+    default: {
+      return state
+    }
+  }
+}
