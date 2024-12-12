e8ed35795438aec477ebef71265d3d6a2c017238
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 18:23:49 -0500

    Rename Error component to ErrorElement for consistency and clarity

--- fast-react-pizza/src/ui/index.ts
+++ fast-react-pizza/src/ui/index.ts
@@ -1,4 +1,4 @@
 export { default as AppLayout } from './AppLayout'
-export { default as Error } from './Error'
+export { default as ErrorElement } from './ErrorElement'
 export { default as Home } from './Home'
 export { default as Loader } from './Loader'
