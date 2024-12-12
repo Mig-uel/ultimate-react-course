6c41d7e5d07b3d4efaf6a7e4a2f187650e065e38
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 17:58:40 -0500

    Export Loader component from ui index for improved accessibility

--- fast-react-pizza/src/ui/index.ts
+++ fast-react-pizza/src/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as AppLayout } from './AppLayout'
 export { default as Error } from './Error'
 export { default as Home } from './Home'
+export { default as Loader } from './Loader'
