60c1b19038eb13012f0d145d229bd583c9de9688
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 17:32:16 -0500

    Refactor routing structure to use AppLayout and organize child routes

--- fast-react-pizza/src/App.tsx
+++ fast-react-pizza/src/App.tsx
@@ -5,27 +5,35 @@ import Menu from './features/menu/menu'
 import Cart from './features/cart/cart'
 import Order from './features/order/order'
 import CreateOrder from './features/order/create-order'
+import AppLayout from './ui/AppLayout'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <Order />,
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
   },
 ])
 
