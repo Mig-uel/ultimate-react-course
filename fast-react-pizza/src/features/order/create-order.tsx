1aee91c4b1bbf0161fb8cac49e16c0239b190108
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 19:59:14 -0500

    Refactor CreateOrder component to use Form from react-router-dom and update priority checkbox handling

--- fast-react-pizza/src/features/order/create-order.tsx
+++ fast-react-pizza/src/features/order/create-order.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Form } from 'react-router-dom'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str: string) =>
@@ -31,14 +32,14 @@ const fakeCart = [
 ]
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false)
   const cart = fakeCart
 
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method='POST'>
         <div>
           <label>First Name</label>
           <input type='text' name='customer' required />
@@ -63,16 +64,16 @@ function CreateOrder() {
             type='checkbox'
             name='priority'
             id='priority'
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            checked={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor='priority'>Want to yo give your order priority?</label>
+          <label htmlFor='priority'>Prioritize Order? (Fees apply)</label>
         </div>
 
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   )
 }
