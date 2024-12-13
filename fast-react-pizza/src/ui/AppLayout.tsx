01c44cb5f58747af1ac98a146fa7b8f7f43abf4b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-13 16:27:05 -0500

    style: refactor AppLayout component to use Tailwind CSS grid layout for improved structure and responsiveness

--- fast-react-pizza/src/ui/AppLayout.tsx
+++ fast-react-pizza/src/ui/AppLayout.tsx
@@ -4,12 +4,14 @@ import CartOverview from '../features/cart/cart-overview'
 
 const AppLayout = () => {
   return (
-    <div className='layout'>
+    <div className='grid h-screen grid-rows-[auto_1fr_auto]'>
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className='overflow-y-scroll'>
+        <main className='mx-auto max-w-3xl'>
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
