b413473e318c73c6ee34cd7d2ac646eee4038ebf
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-13 11:35:24 -0500

    feat: add phone number validation to orderAction and handle errors

--- fast-react-pizza/src/features/order/action.ts
+++ fast-react-pizza/src/features/order/action.ts
@@ -2,7 +2,14 @@ import { type ActionFunctionArgs, redirect } from 'react-router-dom'
 import { createOrder } from '../../services/api_restaurant'
 import type { CartItem, FormOrderItem, OrderItem } from '../../types'
 
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str: string) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  )
+
 async function orderAction({ request }: ActionFunctionArgs) {
+  const errors: Record<string, string> = {}
   const formData: FormData = await request.formData()
 
   const formDataObj = Object.fromEntries(formData.entries()) as Record<
@@ -18,6 +25,12 @@ async function orderAction({ request }: ActionFunctionArgs) {
     phone: formDataObj.phone,
     priority: formDataObj.priority === 'on',
   }
+
+  if (!isValidPhone(order.phone)) {
+    errors.phone = 'Invalid phone number.'
+  }
+
+  if (Object.keys(errors).length > 0) return errors
 
   const placedOrder = (await createOrder(order)) as OrderItem
 
