2ed2153b555e5c74e406d76bc5599da12180f5d2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 19:59:18 -0500

    Refactor orderLoader to use default export and streamline parameter handling

--- fast-react-pizza/src/features/order/loader.ts
+++ fast-react-pizza/src/features/order/loader.ts
@@ -1,10 +1,8 @@
 import { getOrderById } from '../../services/api_restaurant'
 import type { Params } from 'react-router-dom'
 
-export const orderLoader = async ({
-  params,
-}: {
-  params: Params<'orderId'>
-}) => {
+const orderLoader = async ({ params }: { params: Params<'orderId'> }) => {
   return await getOrderById(params.orderId!)
 }
+
+export default orderLoader
