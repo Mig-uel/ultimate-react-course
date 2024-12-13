0b564b6abe66eb1f24807f34c4c7f41dde66d102
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-12 19:25:06 -0500

    Refactor Order component to use useLoaderData and update type to OrderItem

--- fast-react-pizza/src/features/order/order.tsx
+++ fast-react-pizza/src/features/order/order.tsx
@@ -1,58 +1,19 @@
-// Test ID: IIDSAT
+import { useLoaderData } from 'react-router-dom'
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utilities/helpers'
-import type { Order } from '../../types'
-
-const order: Order = {
-  id: 'ABCDEF',
-  customer: 'Jonas',
-  phone: '123456789',
-  address: 'Arroios, Lisbon , Portugal',
-  priority: true,
-  estimatedDelivery: '2027-04-25T10:00:00',
-  cart: [
-    {
-      pizzaId: 7,
-      name: 'Napoli',
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: 'Diavola',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: 'Romana',
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: '-9.000,38.000',
-  orderPrice: 95,
-  priorityPrice: 19,
-}
+import type * as types from '../../types'
 
 function Order() {
+  const order: types.OrderItem = useLoaderData()
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order
-  const deliveryIn = calcMinutesLeft(estimatedDelivery)
+  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
+    order
+
+  const deliveryIn = calcMinutesLeft(estimatedDelivery as Date)
 
   return (
     <div>
@@ -68,10 +29,12 @@ function Order() {
       <div>
         <p>
           {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            ? `Only ${calcMinutesLeft(
+                estimatedDelivery as Date
+              )} minutes left 😃`
             : 'Order should have arrived'}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p>(Estimated delivery: {formatDate(estimatedDelivery as Date)})</p>
       </div>
 
       <div>
