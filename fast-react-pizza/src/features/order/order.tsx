14e0e91462325a746151d17b90eb00f154d97974
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-14 13:14:41 -0500

    style: enhance Order component layout and styling with Tailwind CSS, add order ID display, and improve delivery and receipt sections for better user experience

--- fast-react-pizza/src/features/order/order.tsx
+++ fast-react-pizza/src/features/order/order.tsx
@@ -5,42 +5,77 @@ import {
   formatDate,
 } from '../../utilities/helpers'
 import type * as types from '../../types'
+import OrderItem from './order-item'
 
 function Order() {
   const order: types.OrderItem = useLoaderData()
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
-    order
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order
 
   const deliveryIn = calcMinutesLeft(estimatedDelivery as Date)
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className='space-y-12 px-4 py-6'>
+      <div className='flex flex-wrap items-center justify-between gap-5'>
+        <h2 className='text-xl font-semibold'>Order #{id} Status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className='space-x-2 text-sm font-semibold uppercase'>
+          {priority && (
+            <span className='rounded-full bg-yellow-500 px-3 py-1 text-yellow-50'>
+              Priority
+            </span>
+          )}
+          <span className='rounded-full bg-green-500 px-3 py-1 text-green-50'>
+            {status}
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(
-                estimatedDelivery as Date
-              )} minutes left 😃`
-            : 'Order should have arrived'}
-        </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery as Date)})</p>
+      <div className='space-y-2 bg-stone-200 p-6'>
+        <h2 className='text-lg font-semibold'>Delivery Info</h2>
+
+        <div className='flex flex-wrap justify-between gap-5'>
+          <p>
+            {deliveryIn >= 0
+              ? `Only ${calcMinutesLeft(
+                  estimatedDelivery as Date,
+                )} minutes left 😃`
+              : 'Order has been delivered.'}
+          </p>
+          <p className='text-xs text-stone-500'>
+            (Estimated delivery: {formatDate(estimatedDelivery as Date)})
+          </p>
+        </div>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <ul className='divide-y divide-stone-200 border-b border-t'>
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className='space-y-4 bg-stone-200 p-6'>
+        <h2 className='text-lg font-semibold'>Receipt</h2>
+        <p className='text-sm font-medium text-stone-600'>
+          Subtotal: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className='text-sm font-medium text-stone-600'>
+            Priority Fee: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className='font-bold'>
+          Total: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   )
