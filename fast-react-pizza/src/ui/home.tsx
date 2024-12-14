f203ac97e75cd41d8dd85d251069c1594bd2c442
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-14 14:46:34 -0500

    feat: update Home component to conditionally render CreateUser or Continue ordering button based on username from Redux state

--- fast-react-pizza/src/ui/home.tsx
+++ fast-react-pizza/src/ui/home.tsx
@@ -1,6 +1,10 @@
 import CreateUser from '../features/user/create-user'
+import { useAppSelector } from '../hooks'
+import Button from './Button'
 
 function Home() {
+  const username = useAppSelector((state) => state.user.username)
+
   return (
     <div className='my-10 px-4 text-center md:my-16'>
       <h1 className='mb-8 text-xl font-semibold md:text-3xl'>
@@ -11,7 +15,11 @@ function Home() {
         </span>
       </h1>
 
-      <CreateUser />
+      {username ? (
+        <Button to='/menu'>Continue ordering, {username}</Button>
+      ) : (
+        <CreateUser />
+      )}
     </div>
   )
 }
