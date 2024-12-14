a96ba063aaedb9ec8c6d89cfb0daefec6f71fdcc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-14 13:37:00 -0500

    refactor: update comments in store.ts for clarity on inferred types

--- fast-react-pizza/src/store.ts
+++ fast-react-pizza/src/store.ts
@@ -11,7 +11,9 @@ export default store
 
 // Get the type of our store variable
 export type AppStore = typeof store
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
+// Inferred type
 export type AppDispatch = AppStore['dispatch']
