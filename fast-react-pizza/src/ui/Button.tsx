0fe20cdd146ec99614daee87d82d2e91520ba1fe
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-13 20:45:14 -0500

    style: extend Button component to include 'secondary' type for improved styling options and consistency with Tailwind CSS

--- fast-react-pizza/src/ui/Button.tsx
+++ fast-react-pizza/src/ui/Button.tsx
@@ -11,7 +11,7 @@ const Button = ({
   disabled?: boolean
   to?: string
   onClick?: () => void
-  type?: 'primary' | 'small'
+  type?: 'primary' | 'small' | 'secondary'
 }) => {
   const base =
     'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400'
