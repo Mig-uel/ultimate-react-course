b5d53826c353c3b379339ea2cc858de628e34cf4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-13 19:22:13 -0500

    style: refactor Button component to support multiple styles ('primary' and 'small') with Tailwind CSS for improved flexibility and visual consistency

--- fast-react-pizza/src/ui/Button.tsx
+++ fast-react-pizza/src/ui/Button.tsx
@@ -5,24 +5,31 @@ const Button = ({
   disabled = false,
   to = '',
   onClick = () => {},
+  type = 'primary',
 }: {
   children: React.ReactNode
   disabled?: boolean
   to?: string
   onClick?: () => void
+  type?: 'primary' | 'small'
 }) => {
-  const className =
-    'mt-2 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400 sm:px-6 sm:py-4'
+  const base =
+    'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400'
+
+  const styles = {
+    primary: base + ' md:px-6 md:py-4 px-4 py-3',
+    small: base + ' text-xs px-4 py-2 md:px-5 md:py-2.5',
+  }
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     )
 
   return (
-    <button disabled={disabled} className={className} onClick={onClick}>
+    <button disabled={disabled} className={styles[type]} onClick={onClick}>
       {children}
     </button>
   )
