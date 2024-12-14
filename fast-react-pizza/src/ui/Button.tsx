3f75213c1eb53efd5ebe3c2e3aa82983bd2d2b59
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-14 10:58:51 -0500

    style: update Button component to replace 'text-sn' with 'text-sm' for improved text sizing consistency with Tailwind CSS

--- fast-react-pizza/src/ui/Button.tsx
+++ fast-react-pizza/src/ui/Button.tsx
@@ -20,7 +20,7 @@ const Button = ({
     primary: base + ' md:px-6 md:py-4 px-4 py-3',
     small: base + ' text-xs px-4 py-2 md:px-5 md:py-2.5',
     secondary:
-      'inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400 text-xs px-4 py-2 md:px-5 md:py-2.5 hover:text-stone-800 focus:text-stone-800 text-sn',
+      'inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400 px-4 py-2 md:px-5 md:py-2.5 hover:text-stone-800 focus:text-stone-800 text-sm',
   }
 
   if (to)
