b01d87556d455f9e6b2fc13dc12f200dd7a33c4d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-17 14:43:59 -0500

    feat: enhance CartOverview component to display dynamic cart quantity and total using Redux state

--- fast-react-pizza/src/features/cart/cart-overview.tsx
+++ fast-react-pizza/src/features/cart/cart-overview.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom'
+import { useAppSelector } from '../../hooks'
+import { getCartQty, getCartTotal } from './cartSlice'
+import { formatCurrency } from '../../utilities/helpers'
 
 function CartOverview() {
+  const itemsQty = useAppSelector(getCartQty)
+  const cartTotal = useAppSelector(getCartTotal)
+
+  if (!itemsQty) return null
+
   return (
     <div className='flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6'>
       <p className='space-x-4 text-sm font-semibold text-stone-300 md:text-base'>
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>
+          {itemsQty} {itemsQty > 1 ? 'pizzas' : 'pizza'}
+        </span>
+
+        <span>{formatCurrency(cartTotal)}</span>
       </p>
 
       <Link to='/cart'>Open cart &rarr;</Link>
