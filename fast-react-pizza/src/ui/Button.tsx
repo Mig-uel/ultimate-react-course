6925e0794279cba7e102b73464b23b2083293764
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-17 14:58:16 -0500

    feat: add 'secondary_small' button type to Button component for enhanced styling options

--- fast-react-pizza/src/ui/Button.tsx
+++ fast-react-pizza/src/ui/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({
   disabled?: boolean
   to?: string
   onClick?: () => void
-  type?: 'primary' | 'small' | 'secondary'
+  type?: 'primary' | 'small' | 'secondary' | 'secondary_small'
   buttonType?: HTMLButtonElement['type']
 }) => {
   const base =
@@ -23,6 +23,8 @@ const Button = ({
     small: base + ' text-xs px-4 py-2 md:px-5 md:py-2.5',
     secondary:
       'inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400 px-4 py-2 md:px-5 md:py-2.5 hover:text-stone-800 focus:text-stone-800 text-sm',
+    secondary_small:
+      'inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-400 hover:text-stone-800 focus:text-stone-800 text-xs px-4 py-2 md:px-5 md:py-2.5 ',
   }
 
   if (to)
