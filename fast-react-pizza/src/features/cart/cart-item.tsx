4d21e1e26cdc8b4ac8ac732ebfb56cedbb8255b8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-17 15:17:29 -0500

    refactor: update CartItem component to use RemoveItem for item removal and include pizzaId in props

--- fast-react-pizza/src/features/cart/cart-item.tsx
+++ fast-react-pizza/src/features/cart/cart-item.tsx
@@ -1,9 +1,9 @@
 import { formatCurrency } from '../../utilities/helpers'
 import type { CartItem } from '../../types'
-import Button from '../../ui/Button'
+import RemoveItem from './remove-item'
 
 function CartItem({ item }: { item: CartItem }) {
-  const { name, quantity, totalPrice } = item
+  const { name, quantity, totalPrice, pizzaId } = item
 
   return (
     <li className='space-y-2 py-3 sm:flex sm:items-center sm:justify-between sm:space-y-0'>
@@ -13,8 +13,9 @@ function CartItem({ item }: { item: CartItem }) {
 
       <div className='flex items-center justify-between sm:gap-6'>
         <p className='text-sm font-bold'>{formatCurrency(totalPrice)}</p>
-        <Button type='small'>Remove</Button>
       </div>
+
+      <RemoveItem pizzaId={pizzaId} />
     </li>
   )
 }
