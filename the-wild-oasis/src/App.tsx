e49b4995a1c04a2fc02235e0c3f7e09756bde2c5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-18 14:04:57 -0500

    feat: integrate AppLayout for improved routing structure
    - Wrapped existing routes in the App component with the new AppLayout component to enhance the overall layout and structure of the application.
    - This change promotes a consistent layout across different pages, improving user experience and maintainability of the routing logic.

--- the-wild-oasis/src/App.tsx
+++ the-wild-oasis/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 /* Pages */
+import AppLayout from './ui/AppLayout'
 import Dashboard from './pages/Dashboard'
 import Bookings from './pages/Bookings'
 import Cabins from './pages/Cabins'
@@ -14,13 +15,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate replace to='/dashboard' />} />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='bookings' element={<Bookings />} />
-        <Route path='cabins' element={<Cabins />} />
-        <Route path='users' element={<Users />} />
-        <Route path='settings' element={<Settings />} />
-        <Route path='account' element={<Account />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+        </Route>
+
         <Route path='login' element={<Login />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
