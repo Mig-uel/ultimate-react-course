096463b89a4b38f1175fdbdee5c5fc36486e0e60
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-18 13:28:31 -0500

    feat: enhance Account page with user data and password update sections
    - Updated the Account component to include sections for updating user data and password.
    - Added structured headings and descriptive text to improve user experience and clarity.
    - Utilized the new Row and Heading components for consistent layout and typography.

--- the-wild-oasis/src/pages/Account.tsx
+++ the-wild-oasis/src/pages/Account.tsx
@@ -1,4 +1,20 @@
+import { Heading, Row } from '../ui'
+
 const Account = () => {
-  return <div>Account</div>
+  return (
+    <>
+      <Heading as='h1'>Update Your Account</Heading>
+
+      <Row>
+        <Heading as='h3'>Update User Data</Heading>
+        <p>Update user data form</p>
+      </Row>
+
+      <Row>
+        <Heading as='h3'>Update Password</Heading>
+        <p>Update user password form</p>
+      </Row>
+    </>
+  )
 }
 export default Account
