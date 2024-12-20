31f5107b48082ccab469bcc73adb8f6a46218892
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-19 19:10:48 -0500

    fix: improve error handling in getCabins and deleteCabin functions
    - Updated error handling in api_cabins.ts to log the error message before throwing a new error with a more user-friendly message.
    - This change enhances the clarity of error reporting, making it easier to diagnose issues related to cabin data loading and deletion.

--- the-wild-oasis/src/services/api_cabins.ts
+++ the-wild-oasis/src/services/api_cabins.ts
@@ -4,8 +4,8 @@ export const getCabins = async () => {
   const { data, error } = await supabase.from('cabins').select('*')
 
   if (error) {
-    console.error('Cabins could not be loaded')
-    throw new Error(error.message)
+    console.error(error.message)
+    throw new Error('Cabins could not be loaded')
   }
 
   return data
@@ -15,8 +15,8 @@ export const deleteCabin = async (id: number) => {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id)
 
   if (error) {
-    console.error('Cabin could not be deleted')
-    throw new Error(error.message)
+    console.error(error.message)
+    throw new Error('Cabin could not be deleted')
   }
 
   return data
