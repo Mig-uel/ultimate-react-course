6936a87e8c945eb3b6bed6549b7b58ed4edd06e5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-20 19:22:52 -0500

    feat: enhance createCabin function to support image uploads and improve error handling
    - Updated createCabin function to handle image uploads to Supabase storage, generating a public URL for the cabin image.
    - Improved error handling by removing the uploaded image if cabin creation fails, ensuring no orphaned files remain in storage.
    - Enhanced type safety by modifying the cabin parameter to include an image file, aligning with recent changes in the CabinForm component.

--- the-wild-oasis/src/services/api_cabins.ts
+++ the-wild-oasis/src/services/api_cabins.ts
@@ -12,10 +12,39 @@ export const getCabins = async () => {
   return data
 }
 
-export const createCabin = async (cabin: Tables<'cabins'>) => {
-  const { data, error } = await supabase.from('cabins').insert([cabin])
+export const createCabin = async (
+  cabin: Omit<Tables<'cabins'>, 'image'> & { image: File }
+) => {
+  const imageExtension = cabin.image.name.split('.').pop()
+  const imageName = `${Date.now()}.${imageExtension}`
+
+  // const imagePath = `${
+  //   import.meta.env.VITE_SUPABASE_URL
+  // }/storage/v1/object/public/cabin-images/${imageName}`
+
+  // upload image
+  const { error: bucketError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, cabin.image)
+
+  if (bucketError) {
+    console.error(bucketError.message)
+    throw new Error(bucketError.message)
+  }
+
+  // get image public url
+  const { data: storageData } = supabase.storage
+    .from('cabin-images')
+    .getPublicUrl(imageName)
+
+  // upload cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...cabin, image: storageData.publicUrl }])
 
   if (error) {
+    await supabase.storage.from('cabin-images').remove([imageName])
+
     console.error(error)
     throw new Error('Cabin could not be created')
   }
