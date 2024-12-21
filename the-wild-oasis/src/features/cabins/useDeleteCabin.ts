eb7c2abbde988be92ada576366634af78d15110c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-21 10:10:08 -0500

    feat: add useDeleteCabin hook for cabin deletion functionality
    - Introduced a new custom hook, useDeleteCabin, to encapsulate the cabin deletion logic using react-query.
    - Implemented success and error toast notifications to enhance user feedback during deletion.
    - Integrated query invalidation to ensure the cabin list is updated after a successful deletion, improving data consistency.

---
+++ the-wild-oasis/src/features/cabins/useDeleteCabin.ts
@@ -0,0 +1,28 @@
+import toast from 'react-hot-toast'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteCabin } from '../../services/api_cabins'
+
+export const useDeleteCabin = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteCabin,
+
+    onSuccess() {
+      toast.success('Cabin successfully deleted')
+
+      queryClient.invalidateQueries({
+        queryKey: ['cabins'],
+      })
+    },
+
+    onError(error) {
+      toast.error(error.message)
+    },
+  })
+
+  return {
+    mutate,
+    isPending,
+  }
+}
