fc542f175945421c7e2e532d7fc06c53e144de74
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-22 15:49:55 -0500

    feat: update Settings page to include UpdateSettingsForm component
    - Refactored the Settings page to replace the placeholder text with a structured layout.
    - Added a heading for "Update Hotel Settings" to improve clarity.
    - Integrated the UpdateSettingsForm component, enhancing user interaction for updating hotel settings.
    - This change improves the overall user experience by providing a more functional and visually appealing settings interface.

--- the-wild-oasis/src/pages/Settings.tsx
+++ the-wild-oasis/src/pages/Settings.tsx
@@ -1,4 +1,13 @@
+import { Heading, Row } from '../ui'
+import UpdateSettingsForm from '../features/settings/UpdateSettingsForm'
+
 const Settings = () => {
-  return <div>Settings</div>
+  return (
+    <Row>
+      <Heading as='h1'>Update Hotel Settings</Heading>
+
+      <UpdateSettingsForm />
+    </Row>
+  )
 }
 export default Settings
