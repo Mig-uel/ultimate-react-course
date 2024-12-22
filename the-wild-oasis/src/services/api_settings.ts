0de529cfc28c0dac38454aaea53b4852e8f810b6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-22 17:56:49 -0500

    refactor: update updateSetting function to accept Partial type for settings
    - Modified the updateSetting function to accept a Partial<Tables<'settings'>> type for the newSetting parameter, enhancing flexibility in updating settings.
    - This change improves type safety and allows for partial updates, streamlining the settings management process.

--- the-wild-oasis/src/services/api_settings.ts
+++ the-wild-oasis/src/services/api_settings.ts
@@ -13,7 +13,7 @@ export async function getSettings() {
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
-export async function updateSetting(newSetting: Tables<'settings'>) {
+export async function updateSetting(newSetting: Partial<Tables<'settings'>>) {
   const { data, error } = await supabase
     .from('settings')
     .update(newSetting)
