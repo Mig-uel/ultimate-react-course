5bd08477421e462ba4a16c2800dcf5628e05621a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-24 22:25:38 -0500

    feat: implement Modal component with close functionality and styled design
    - Added a new Modal component that utilizes styled-components for consistent styling.
    - Integrated a close button using the HiXMark icon from react-icons for improved user interaction.
    - The Modal component accepts children and an onClose prop, allowing for flexible content and closure handling.
    - This addition enhances the user interface by providing a reusable modal structure for various use cases.

--- the-wild-oasis/src/ui/Modal.tsx
+++ the-wild-oasis/src/ui/Modal.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { HiXMark } from 'react-icons/hi2'
 
 const StyledModal = styled.div`
   position: fixed;
@@ -10,7 +11,7 @@ const StyledModal = styled.div`
   box-shadow: var(--shadow-lg);
   padding: 3.2rem 4rem;
   transition: all 0.5s;
-`;
+`
 
 const Overlay = styled.div`
   position: fixed;
@@ -22,7 +23,7 @@ const Overlay = styled.div`
   backdrop-filter: blur(4px);
   z-index: 1000;
   transition: all 0.5s;
-`;
+`
 
 const Button = styled.button`
   background: none;
@@ -47,4 +48,24 @@ const Button = styled.button`
     stroke: var(--color-grey-500); */
     color: var(--color-grey-500);
   }
-`;
+`
+
+const Modal = ({
+  children,
+  onClose,
+}: {
+  children: React.ReactNode
+  onClose: () => void
+}) => {
+  return (
+    <Overlay>
+      <StyledModal>
+        <Button onClick={onClose}>
+          <HiXMark />
+        </Button>
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>
+  )
+}
+export default Modal
