8840447293a61102de71d06eff0842ee49e7e4ea
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-25 18:49:33 -0500

    refactor: simplify AddCabin component with integrated modal structure
    - Replaced local state management for modal visibility with a more streamlined modal structure using the updated Modal component.
    - Integrated CabinForm directly within the Modal, enhancing the user experience by providing a cohesive interface for adding new cabins.
    - This change improves code clarity and user interaction by utilizing the new Modal API for better organization and functionality.

--- the-wild-oasis/src/features/cabins/AddCabin.tsx
+++ the-wild-oasis/src/features/cabins/AddCabin.tsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
 import { Button } from '../../ui'
 import Modal from '../../ui/Modal'
 import CabinForm from './CabinForm'
 
 const AddCabin = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const handleModalClose = () => setIsModalOpen((prev) => !prev)
-
   return (
-    <div>
-      <Button onClick={() => setIsModalOpen((prev) => !prev)}>
-        Add New Cabin
-      </Button>
-
-      {isModalOpen && (
-        <Modal onClose={handleModalClose}>
-          <CabinForm onCloseModal={handleModalClose} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens='form'>
+        <Button>Add New Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name='form'>
+        <CabinForm />
+      </Modal.Window>
+    </Modal>
   )
 }
+
 export default AddCabin
