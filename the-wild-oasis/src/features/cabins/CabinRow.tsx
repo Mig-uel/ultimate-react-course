0d11f1db4ee21864a4f4106bb70486cea6901ce4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-27 17:30:27 -0500

    refactor: update CabinRow component to use Table for improved structure
    - Replaced custom TableRow styled component with the Table component from the UI library for better consistency and maintainability.
    - Adjusted the rendering of cabin images and details to align with the new Table structure, enhancing the overall layout.
    - This refactor streamlines the CabinRow component, improving code organization and user experience.

--- the-wild-oasis/src/features/cabins/CabinRow.tsx
+++ the-wild-oasis/src/features/cabins/CabinRow.tsx
@@ -7,18 +7,7 @@ import CabinForm from './CabinForm'
 import { useCreateCabin } from './useCreateCabin'
 import { useDeleteCabin } from './useDeleteCabin'
 import ConfirmDelete from '../../ui/ConfirmDelete'
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`
+import Table from '../../ui/Table'
 
 const Img = styled.img`
   display: block;
@@ -63,7 +52,7 @@ const CabinRow = ({ cabin }: { cabin: Tables<'cabins'> }) => {
   }
 
   return (
-    <TableRow role='row'>
+    <Table.Row>
       <Img src={image!} />
       <Cabin>{name}</Cabin>
 
@@ -109,7 +98,7 @@ const CabinRow = ({ cabin }: { cabin: Tables<'cabins'> }) => {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   )
 }
 export default CabinRow
