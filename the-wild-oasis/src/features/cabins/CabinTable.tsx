9299c5fd484e3efddd5cbd7ac55da4a81e41a334
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-27 17:30:32 -0500

    refactor: simplify CabinTable component by integrating UI Table component
    - Replaced custom styled Table and TableHeader with the Table component from the UI library for improved consistency and maintainability.
    - Updated the CabinTable rendering logic to align with the new Table structure, enhancing overall layout and readability.
    - This refactor streamlines the CabinTable component, improving code organization and user experience.

--- the-wild-oasis/src/features/cabins/CabinTable.tsx
+++ the-wild-oasis/src/features/cabins/CabinTable.tsx
@@ -1,40 +1,17 @@
-import styled from 'styled-components'
 import { useCabins } from './useCabins'
 import CabinRow from './CabinRow'
 import { Spinner } from '../../ui/'
+import Table from '../../ui/Table'
 import { HiTableCells } from 'react-icons/hi2'
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`
 const CabinTable = () => {
   const { cabins, isLoading } = useCabins()
 
   if (isLoading) return <Spinner />
 
   return (
-    <Table role='table'>
-      <TableHeader role='row'>
+    <Table cols='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+      <Table.Header>
         <div>
           <HiTableCells />
         </div>
@@ -43,7 +20,7 @@ const CabinTable = () => {
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
+      </Table.Header>
 
       {cabins?.map((cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
