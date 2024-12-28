b1015988f63463192b8c0d58fc96b1d445d62f4e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-28 03:03:19 -0500

    feat: integrate BookingTable component into Bookings page layout
    - Updated the Bookings page to include the BookingTable component, enhancing the display of booking information.
    - Refactored the layout to improve structure and readability, ensuring a more organized presentation of bookings alongside the existing heading and placeholder text.

--- the-wild-oasis/src/pages/Bookings.tsx
+++ the-wild-oasis/src/pages/Bookings.tsx
@@ -1,12 +1,17 @@
+import BookingTable from '../features/bookings/BookingTable'
 import { Heading, Row } from '../ui'
 
 const Bookings = () => {
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All Bookings</Heading>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All Bookings</Heading>
 
-      <p>placeholder</p>
-    </Row>
+        <p>placeholder</p>
+      </Row>
+
+      <BookingTable />
+    </>
   )
 }
 export default Bookings
