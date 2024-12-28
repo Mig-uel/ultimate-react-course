a787a6b7dd44087a21147f08ae82c121d31177ee
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-27 22:55:30 -0500

    feat: add filtering functionality to CabinTable for cabin discounts
    - Implemented filtering logic in CabinTable to allow users to view cabins based on discount status (all, no-discount, with-discount).
    - Integrated useSearchParams from react-router-dom to manage filter state, enhancing user experience by providing relevant cabin listings.
    - This update improves the usability of the CabinTable component, enabling more efficient cabin management and selection.

--- the-wild-oasis/src/features/cabins/CabinTable.tsx
+++ the-wild-oasis/src/features/cabins/CabinTable.tsx
@@ -3,11 +3,23 @@ import CabinRow from './CabinRow'
 import { Menus, Spinner, Table } from '../../ui/'
 import { HiTableCells } from 'react-icons/hi2'
 import { Tables } from '../../supabase_types'
+import { useSearchParams } from 'react-router-dom'
 
 const CabinTable = () => {
   const { cabins, isLoading } = useCabins()
+  const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+
+  const filterValue = searchParams.get('discount') || 'all'
+
+  let filteredCabins
+
+  if (filterValue == 'all') filteredCabins = cabins
+  if (filterValue === 'no-discount')
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0)
+  else if (filterValue === 'with-discount')
+    filteredCabins = cabins?.filter((cabin) => cabin.discount)
 
   return (
     <Menus>
@@ -24,7 +36,7 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={cabins as Tables<'cabins'>[]}
+          data={filteredCabins as Tables<'cabins'>[]}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
