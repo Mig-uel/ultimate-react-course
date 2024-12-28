ead05c6d7cc5db143355f3afc33f8194b99e66b2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-27 21:37:19 -0500

    feat: implement Menus component with context for dynamic menu management
    - Introduced a new Menus component that utilizes React context to manage open/close states for menu items, enhancing user interaction.
    - Added Toggle, List, and Button subcomponents to facilitate menu actions, allowing for better organization and usability.
    - Updated styled components to improve positioning and styling consistency.
    - This implementation enhances the overall user experience by providing a cohesive and maintainable menu structure.

--- the-wild-oasis/src/ui/Menus.tsx
+++ the-wild-oasis/src/ui/Menus.tsx
@@ -1,13 +1,16 @@
+import { createContext, useContext, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { HiEllipsisVertical } from 'react-icons/hi2'
 import styled from 'styled-components'
 
 type StyledListProps = {
-  position: {
-    x: string
-    y: string
+  $position: {
+    x: number
+    y: number
   }
 }
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -39,8 +42,8 @@ const StyledList = styled.ul<StyledListProps>`
   box-shadow: var(--shadow-md);
   border-radius: var(--border-radius-md);
 
-  right: ${(props) => props.position.x}px;
-  top: ${(props) => props.position.y}px;
+  right: ${(props) => props.$position.x}px;
+  top: ${(props) => props.$position.y}px;
 `
 
 const StyledButton = styled.button`
@@ -67,3 +70,71 @@ const StyledButton = styled.button`
     transition: all 0.3s;
   }
 `
+
+/** MENU CONTEXT */
+const MenuContext = createContext<{
+  openId: string | number
+  close: () => void
+  open: React.Dispatch<React.SetStateAction<string>>
+}>({
+  close: () => {},
+  open: () => {},
+  openId: '',
+})
+
+/** MENUS PARENT COMPONENT */
+const Menus = ({ children }: { children: React.ReactNode }) => {
+  const [openId, setOpenId] = useState('')
+
+  const close = () => setOpenId('')
+  const open = setOpenId
+
+  const value = {
+    openId,
+    close,
+    open,
+  }
+
+  return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>
+}
+
+/** MENUS TOGGLE COMPONENT */
+const Toggle = ({ id }: { id: number }) => {
+  const { openId, close, open } = useContext(MenuContext)
+
+  const handleClick = () => {
+    openId === '' || +openId !== +id ? open(id) : close()
+  }
+
+  return (
+    <StyledToggle onClick={handleClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  )
+}
+
+const List = ({ id, children }: { id: number; children: React.ReactNode }) => {
+  const { openId } = useContext(MenuContext)
+
+  if (+openId !== +id) return null
+
+  return createPortal(
+    <StyledList $position={{ x: 20, y: 20 }}>{children}</StyledList>,
+    document.body
+  )
+}
+
+const Button = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <li>
+      <StyledButton>{children}</StyledButton>
+    </li>
+  )
+}
+
+Menus.Menu = Menu
+Menus.Toggle = Toggle
+Menus.List = List
+Menus.Button = Button
+
+export default Menus
