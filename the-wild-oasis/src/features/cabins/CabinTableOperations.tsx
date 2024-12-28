e22c97793f6039352c5e132cbe31808ddc00205f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-28 01:29:41 -0500

    feat: add sorting functionality to CabinTableOperations
    - Introduced a new SortBy component in CabinTableOperations to allow users to sort cabin listings by various criteria including name and price.
    - Enhanced user experience by providing options for sorting cabins in both ascending and descending order based on name and capacity.
    - This addition complements the existing filtering capabilities, offering a more comprehensive management interface for cabin listings.

--- the-wild-oasis/src/features/cabins/CabinTableOperations.tsx
+++ the-wild-oasis/src/features/cabins/CabinTableOperations.tsx
@@ -1,4 +1,4 @@
-import { Filter, TableOperations } from '../../ui'
+import { Filter, SortBy, TableOperations } from '../../ui'
 
 const CabinTableOperations = () => {
   return (
@@ -9,6 +9,32 @@ const CabinTableOperations = () => {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No Discount' },
           { value: 'with-discount', label: 'With Discount' },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { label: 'Name (A-Z)', value: 'a-z' },
+          {
+            label: 'Name (Z-A)',
+            value: 'z-a',
+          },
+          {
+            label: 'Price (low-high)',
+            value: 'regularPrice-asc',
+          },
+          {
+            label: 'Price (high-low)',
+            value: 'regularPrice-desc',
+          },
+          {
+            label: 'Capacity (low-high)',
+            value: 'maxCapacity-asc',
+          },
+          {
+            label: 'Capacity (high-low)',
+            value: 'maxCapacity-desc',
+          },
         ]}
       />
     </TableOperations>
