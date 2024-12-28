8c94b903da6c641408342a377203f96e2d56448b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-28 01:29:56 -0500

    feat: refactor Select component to use styled props and enhance functionality
    - Updated the StyledSelect component to use a new prop naming convention, changing 'type' to '$type' for better clarity and consistency.
    - Introduced a new Select component that accepts options, value, type, and onChange props, improving the component's usability and integration within the UI.
    - This enhancement streamlines the selection process and improves the overall user experience by providing a more flexible and maintainable Select component.

--- the-wild-oasis/src/ui/Select.tsx
+++ the-wild-oasis/src/ui/Select.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 type StyledSelectProps = {
-  type: string
+  $type: string
 }
 
 const StyledSelect = styled.select<StyledSelectProps>`
@@ -9,7 +9,7 @@ const StyledSelect = styled.select<StyledSelectProps>`
   padding: 0.8rem 1.2rem;
   border: 1px solid
     ${(props) =>
-      props.type === 'white'
+      props.$type === 'white'
         ? 'var(--color-grey-100)'
         : 'var(--color-grey-300)'};
   border-radius: var(--border-radius-sm);
@@ -17,3 +17,26 @@ const StyledSelect = styled.select<StyledSelectProps>`
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `
+
+const Select = ({
+  options,
+  value,
+  type,
+  onChange,
+}: {
+  options: { label: string; value: string }[]
+  value: string
+  type: string
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}) => {
+  return (
+    <StyledSelect value={value} $type={type} onChange={onChange}>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  )
+}
+export default Select
