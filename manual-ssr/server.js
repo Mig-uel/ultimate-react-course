97cf9939280e2131aa27051840dc5ebd12327651
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-28 21:09:26 -0500

    feat: enhance server.js to serve HTML template for root path
    - Updated server.js to read and serve an HTML template from index.html when the root path '/' is requested.
    - Improved response handling by setting the correct content type for HTML responses.
    - Maintained existing functionality for the '/test' path and error handling for unknown URLs.

--- manual-ssr/server.js
+++ manual-ssr/server.js
@@ -1,12 +1,22 @@
-const { createServer } = require('http')
 const { parse } = require('url')
+const { createServer } = require('http')
+const { readFileSync } = require('fs')
+const path = require('path')
+
+const htmlFilePath = path.resolve(__dirname, 'index.html')
+const htmlTemplate = readFileSync(htmlFilePath, 'utf-8')
 
 const port = 5000
 const server = createServer((req, res) => {
   const pathName = parse(req.url, true).pathname
 
-  if (pathName === '/') res.end('Hello, world!')
-  else if (pathName === '/test') res.end('Test')
+  if (pathName === '/') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    })
+
+    res.end(htmlTemplate)
+  } else if (pathName === '/test') res.end('Test')
   else res.end('The URL cannot be found ')
 })
 
