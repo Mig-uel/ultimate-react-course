abf34f2ac2640b8979b55bbc32ca05e91df1bd08
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-28 21:05:44 -0500

    fix: update server.js to correct URL parsing and response handling
    - Changed URL parsing from 'path' to 'url' to accurately retrieve pathname.
    - Updated response conditions to check for root path '/' and '/test' instead of empty string and 'test', improving request handling.
    - Replaced 'log' with 'console.log' for consistency in logging server status.

--- manual-ssr/server.js
+++ manual-ssr/server.js
@@ -1,14 +1,15 @@
-const { log } = require('console')
 const { createServer } = require('http')
-const { parse } = require('path')
+const { parse } = require('url')
 
 const port = 5000
 const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).name
+  const pathName = parse(req.url, true).pathname
 
-  if (pathName === '') res.end('Hello, world!')
-  else if (pathName === 'test') res.end('Test')
+  if (pathName === '/') res.end('Hello, world!')
+  else if (pathName === '/test') res.end('Test')
   else res.end('The URL cannot be found ')
 })
 
-server.listen(port, () => log(`Listening for requests on port: ${port}`))
+server.listen(port, () =>
+  console.log(`Listening for requests on port: ${port}`)
+)
