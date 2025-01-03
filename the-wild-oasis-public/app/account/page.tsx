ed2adbfd14591e0e28cb0938812a4ac83caa9cb6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-03 02:10:25 -0500

    feat: update AccountPage to welcome users with a personalized message
    - Changed the AccountPage component to display a welcoming message "Welcome, Guest!" instead of a generic "Account" text.
    - Enhanced the visual presentation by applying styling classes for improved user experience.

--- the-wild-oasis-public/app/account/page.tsx
+++ the-wild-oasis-public/app/account/page.tsx
@@ -5,6 +5,10 @@ export const metadata: Metadata = {
 }
 
 const AccountPage = () => {
-  return <div>Account</div>
+  return (
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome, Guest!
+    </h2>
+  )
 }
 export default AccountPage
