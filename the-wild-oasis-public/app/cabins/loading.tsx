87e2cb51507dc36f12c89ecbaf8e3af2d13199fe
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-03 03:08:31 -0500

    feat: add loading component for cabins page
    - Introduced a new Loading component in loading.tsx to enhance user experience during data fetching.
    - The component displays a spinner and a loading message, improving visual feedback while cabins data is being loaded.

---
+++ the-wild-oasis-public/app/cabins/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='grid items-center justify-center'>
+      <div className='spinner'></div>
+
+      <p className='text-xl text-primary-200'>Loading cabins...</p>
+    </div>
+  )
+}
