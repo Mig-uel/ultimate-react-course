12d4f35f9bd1a74a51e12c8b8013ea239d9a66de
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-03 02:59:43 -0500

    feat: add remote image patterns to Next.js configuration for Supabase integration
    - Updated next.config.ts to include remote image patterns for loading images from Supabase storage.
    - This change enhances the application's ability to serve images securely from the specified Supabase hostname.

--- the-wild-oasis-public/next.config.ts
+++ the-wild-oasis-public/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'xmabeywyvhrwlxntjxya.supabase.co',
+        pathname: '/storage/v1/object/public/cabin-images/**',
+        protocol: 'https',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
