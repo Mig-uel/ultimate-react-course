af6c6ac056cbd75a96639c65a4815f2d7125c56c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 09:09:09 -0500

    feat: update ReservationForm to accept user prop and display user information

--- the-wild-oasis-public/components/ReservationForm.tsx
+++ the-wild-oasis-public/components/ReservationForm.tsx
@@ -2,8 +2,9 @@
 
 import type { Cabin } from '@/app/types'
 import { useReservationContext } from './ReservationContext'
+import type { User } from 'next-auth'
 
-function ReservationForm({ cabin }: { cabin: Cabin }) {
+function ReservationForm({ cabin, user }: { cabin: Cabin; user: User }) {
   const { range } = useReservationContext()
   const { maxCapacity } = cabin
 
@@ -12,16 +13,15 @@ function ReservationForm({ cabin }: { cabin: Cabin }) {
       <div className='bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center'>
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
           <img
-            // Important to display google profile images
             referrerPolicy='no-referrer'
             className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
+            src={user.image!}
+            alt={user.name!}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
