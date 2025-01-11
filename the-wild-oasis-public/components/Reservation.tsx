19c1d06168eb69a64ef3dca5dd306658a9b44ba1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 05:43:31 -0500

    feat: add DateSelector and ReservationForm components for booking functionality

---
+++ the-wild-oasis-public/components/Reservation.tsx
@@ -0,0 +1,17 @@
+import { getBookedDatesByCabinId, getSettings } from '@/lib/data-service'
+import { DateSelector, ReservationForm } from './'
+import type { Cabin } from '@/app/types'
+
+export default async function Reservation({ cabin }: { cabin: Cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ])
+
+  return (
+    <div className='grid grid-cols-2 border border-primary-800 min-h-[400px] mt-10'>
+      <DateSelector />
+      <ReservationForm />
+    </div>
+  )
+}
