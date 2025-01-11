459fe47213fd81374ba882c327beb1691af8723d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 12:55:53 -0500

    feat: personalize account page greeting with user's first name

--- the-wild-oasis-public/app/account/page.tsx
+++ the-wild-oasis-public/app/account/page.tsx
@@ -1,13 +1,18 @@
+import { auth } from '@/lib/auth'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Account',
 }
 
-const AccountPage = () => {
+const AccountPage = async () => {
+  const session = await auth()
+
+  const firstName = session?.user?.name?.split(' ')[0]
+
   return (
     <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
-      Welcome, Guest!
+      Welcome, {firstName}!
     </h2>
   )
 }
