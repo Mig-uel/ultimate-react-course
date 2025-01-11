67a84877365d69c4ea60fda200407b321b92395c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 05:56:44 -0500

    fix: update cabin name reference in reservation heading for accurate display

--- the-wild-oasis-public/app/cabins/[id]/page.tsx
+++ the-wild-oasis-public/app/cabins/[id]/page.tsx
@@ -32,16 +32,13 @@ export const generateStaticParams = async () => {
 export default async function Page({ params }: Props) {
   const cabin = await getCabin((await params).id)
 
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin
-
   return (
     <div className='max-w-6xl mx-auto mt-8'>
       <Cabin cabin={cabin} />
 
       <div>
         <h2 className='text-5xl font-semibold text-center text-accent-400'>
-          Reserve {name} today. Pay on arrival.
+          Reserve {cabin.name} today. Pay on arrival.
         </h2>
 
         <Suspense fallback={<Spinner />}>
