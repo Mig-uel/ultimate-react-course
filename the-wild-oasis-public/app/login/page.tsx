a230d9c568e51c6800b17ecedc6f82e682ed69b2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 13:02:40 -0500

    feat: update login page heading to reflect account access

--- the-wild-oasis-public/app/login/page.tsx
+++ the-wild-oasis-public/app/login/page.tsx
@@ -9,7 +9,7 @@ export default function Login() {
   return (
     <div className='flex flex-col gap-10 mt-10 items-center'>
       <h2 className='text-3xl font-semibold'>
-        Sign in to access your guest area
+        Sign in to access your account area
       </h2>
 
       <SignInButton />
