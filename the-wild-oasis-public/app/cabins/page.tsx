ee7f76e75b46775e67ae550c57c17a54e0710c25
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 04:46:13 -0500

    fix: add key prop to Suspense component for improved rendering

--- the-wild-oasis-public/app/cabins/page.tsx
+++ the-wild-oasis-public/app/cabins/page.tsx
@@ -31,7 +31,7 @@ const CabinsPage = async ({
         <Filter filter={filter} />
       </div>
 
-      <Suspense fallback={<Spinner />}>
+      <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
       </Suspense>
     </div>
