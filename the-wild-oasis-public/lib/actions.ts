7d82b107a70e2ad6480cb2aea936bd67473d80e2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-11 13:00:36 -0500

    feat: add signOutAction to handle user sign-out with redirect

--- the-wild-oasis-public/lib/actions.ts
+++ the-wild-oasis-public/lib/actions.ts
@@ -1,11 +1,17 @@
 'use server'
 
-import { signIn } from './auth'
+import { signIn, signOut } from './auth'
 
 export async function signInAction(redirect: string) {
   console.log(redirect)
 
   await signIn('google', {
     redirectTo: redirect || '/account',
+  })
+}
+
+export async function signOutAction() {
+  await signOut({
+    redirectTo: '/',
   })
 }
