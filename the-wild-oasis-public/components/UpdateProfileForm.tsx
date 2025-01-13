a20f9286d3bf0b7327ee412a2c2f349c3d0024c8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-13 10:33:23 -0500

    feat: enhance UpdateProfileForm to include guest data and update profile action

--- the-wild-oasis-public/components/UpdateProfileForm.tsx
+++ the-wild-oasis-public/components/UpdateProfileForm.tsx
@@ -1,13 +1,30 @@
 'use client'
 
-const UpdateProfileForm = ({ children }: { children: React.ReactNode }) => {
+import { updateProfile } from '@/lib/actions'
+import type { Guest } from '@/app/types'
+import Image from 'next/image'
+
+const UpdateProfileForm = ({
+  children,
+  guest,
+}: {
+  children: React.ReactNode
+  guest: Guest
+}) => {
+  const { countryFlag, email, fullName, nationalID, nationality } = guest
+
   return (
-    <form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
+    <form
+      action={updateProfile}
+      className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'
+    >
       <div className='space-y-2'>
         <label>Full name</label>
         <input
           disabled
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
+          defaultValue={fullName}
+          name='fullName'
         />
       </div>
 
@@ -16,17 +33,21 @@ const UpdateProfileForm = ({ children }: { children: React.ReactNode }) => {
         <input
           disabled
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
+          defaultValue={email}
+          name='email'
         />
       </div>
 
       <div className='space-y-2'>
         <div className='flex items-center justify-between'>
           <label htmlFor='nationality'>Where are you from?</label>
-          {/* <Image
-        src={countryFlag}
-        alt='Country flag'
-        className='h-5 rounded-sm'
-      /> */}
+          <Image
+            src={countryFlag || ''}
+            alt='Country flag'
+            className='h-5 rounded-sm'
+            width={40}
+            height={40}
+          />
         </div>
 
         <>{children}</>
@@ -37,6 +58,7 @@ const UpdateProfileForm = ({ children }: { children: React.ReactNode }) => {
         <input
           name='nationalID'
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
+          defaultValue={nationalID || ''}
         />
       </div>
 
