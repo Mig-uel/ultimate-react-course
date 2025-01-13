10cdf960e5a4417b16399da518ada928d42fdd7f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-13 10:56:03 -0500

    feat: add key prop to SelectCountry component for improved rendering

--- the-wild-oasis-public/components/SelectCountry.tsx
+++ the-wild-oasis-public/components/SelectCountry.tsx
@@ -27,6 +27,7 @@ async function SelectCountry({
       // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
+      key={defaultCountry}
     >
       <option value=''>Select country...</option>
       {countries.map((c) => (
