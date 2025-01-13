927ab10f6a8bee592331b0004fa688cf7bdd8e90
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-13 10:33:43 -0500

    feat: implement updateProfile function to handle guest profile updates and revalidate path

--- the-wild-oasis-public/lib/actions.ts
+++ the-wild-oasis-public/lib/actions.ts
@@ -1,6 +1,10 @@
 'use server'
 
-import { signIn, signOut } from './auth'
+import { revalidatePath } from 'next/cache'
+import { auth, signIn, signOut } from './auth'
+import { updateGuest } from './data-service'
+
+/** Server Actions */
 
 export async function signInAction(redirect: string) {
   console.log(redirect)
@@ -14,4 +18,26 @@ export async function signOutAction() {
   await signOut({
     redirectTo: '/',
   })
+}
+
+export async function updateProfile(formData: FormData) {
+  try {
+    const session = await auth()
+
+    if (!session || !session.user) throw new Error()
+
+    const { user } = session
+
+    const data = Object.fromEntries(formData) as Record<string, string>
+
+    await updateGuest(user.guestID, {
+      nationality: data.nationality.split('%')[0],
+      nationalID: data.nationalID,
+      countryFlag: data.nationality.split('%')[1],
+    })
+
+    return revalidatePath('/account/profile')
+  } catch (error) {
+    console.log(error)
+  }
 }
