f52b7c0c00c96d3a959ac169ddb2f03425dacaaa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-15 09:43:50 -0500

    feat: implement optimistic deletion in ReservationList component

--- the-wild-oasis-public/components/ReservationList.tsx
+++ the-wild-oasis-public/components/ReservationList.tsx
@@ -3,12 +3,29 @@
 import type { Booking } from '@/app/types'
 import ReservationCard from './ReservationCard'
 import { useOptimistic } from 'react'
+import { deleteReservation } from '@/lib/actions'
 
 export default function ReservationList({ bookings }: { bookings: Booking[] }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currBookings, bookingID: number) => {
+      return currBookings.filter((booking) => booking.id !== bookingID)
+    }
+  )
+
+  async function handleDelete(bookingID: number) {
+    optimisticDelete(bookingID)
+    await deleteReservation(bookingID)
+  }
+
   return (
     <ul className='space-y-6'>
-      {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
       ))}
     </ul>
   )
